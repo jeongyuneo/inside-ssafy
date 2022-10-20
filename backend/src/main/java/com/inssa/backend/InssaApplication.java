9b330bd5268287a98b37c4ec8a704c74f571b22f
jeonyuneo <piq2255@gmail.com>
2022-10-21 02:41:11 +0900

    chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/inssa/backend/InssaApplication.java
@@ -0,0 +1,12 @@
+package com.inssa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InssaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InssaApplication.class, args);
+    }
+}
