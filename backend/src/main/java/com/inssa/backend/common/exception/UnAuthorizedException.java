2ddfd3aef9009026b61e00bf3f5f37e58e63c786
어정윤 <piq2255@naver.com>
2022-10-25 16:33:12 +0900

    feat: UnAuthorizedException 추가

---
+++ backend/src/main/java/com/inssa/backend/common/exception/UnAuthorizedException.java
@@ -0,0 +1,10 @@
+package com.inssa.backend.common.exception;
+
+import com.inssa.backend.common.domain.ErrorMessage;
+
+public class UnAuthorizedException extends CommonException {
+
+    public UnAuthorizedException(ErrorMessage message) {
+        super(message);
+    }
+}
