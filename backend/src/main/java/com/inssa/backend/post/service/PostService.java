1d0d2dc300db623fc06d04989bb8317ad8e196c1
jeonyuneo <piq2255@gmail.com>
2022-10-26 01:42:36 +0900

    feat: PostService - 익명 게시판 목록 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/service/PostService.java
+++ backend/src/main/java/com/inssa/backend/post/service/PostService.java
@@ -1,7 +1,14 @@
 package com.inssa.backend.post.service;
 
+import com.inssa.backend.post.controller.dto.PostsResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+
+    public List<PostsResponse> getPosts() {
+        return null;
+    }
 }
