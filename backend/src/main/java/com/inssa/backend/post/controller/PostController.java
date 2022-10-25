85b537621819c65e57517c926548a733eaa3f8dd
jeonyuneo <piq2255@gmail.com>
2022-10-26 01:45:58 +0900

    feat: PostController - 익명 게시판 목록 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/controller/PostController.java
+++ backend/src/main/java/com/inssa/backend/post/controller/PostController.java
@@ -1,9 +1,24 @@
 package com.inssa.backend.post.controller;
 
+import com.inssa.backend.post.controller.dto.PostsResponse;
+import com.inssa.backend.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<PostsResponse>> getPosts() {
+        return ResponseEntity.ok().body(postService.getPosts());
+    }
 }
