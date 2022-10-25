1fb5600476e56c55c6cd7b5b899042cc9e005b37
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-10-26 00:18:38 +0900

    feat: 메인 페이지 ui 구현

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
+const App = () =>  {
+
+  const busNumber = [1,2,3,4,5,6];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    {busNumber.map(num =>
+    <TouchableOpacity
+    key={num}
+      onPress={() => Alert.alert(`${num}호차`)}
+      style={styles.button}>
+      <Text style={{ fontSize: 100, color: '#fff' }}>{num}</Text>
+    </TouchableOpacity> )}
+    <StatusBar style="auto" />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    alignContent: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    // padding: 10
+  },
+  button: {
+    width: 150,
+    height: 200,
+    backgroundColor: '#01A7EB',
+    margin: 10,
+    justifyContent: 'center',
+    alignItems:"center"
   },
 });
+
+export default App;
