a7eb65f2de3fbc7e836829bb02aa29c42e4f936f
soobin <asdfls@naver.com>
2022-10-27 00:59:31 +0900

    feat: MemberController 추가

---
+++ backend/src/main/java/com/inssa/backend/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.inssa.backend.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+}
