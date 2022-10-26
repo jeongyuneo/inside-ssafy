dd7ff6a8aab6d05b8103d46c46de5ffee63dd4bc
jjong <jj_jjong@naver.com>
2022-10-27 03:07:04 +0900

    feat: MenuControllerTest 생성

---
+++ backend/src/test/java/com/inssa/backend/menu/controller/MenuControllerTest.java
@@ -0,0 +1,8 @@
+package com.inssa.backend.menu.controller;
+
+import com.inssa.backend.ApiDocument;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MenuController.class)
+public class MenuControllerTest extends ApiDocument {
+}
