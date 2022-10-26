0f28d691989a2e4a8bafdd21124339d22354be6f
jeonyuneo <piq2255@gmail.com>
2022-10-26 13:43:27 +0900

    feat: ErrorMessage - 익명 게시판 상세 조회 실패 메시지 추가

--- backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorMessage {
 
+    NOT_FOUND_POST("해당 게시글을 찾을 수 없습니다."),
+
     FAIL_TO_GET_POSTS("익명 게시판 목록 조회에 실패했습니다."),
 
     INTERNAL_SERVER_ERROR("서버 내부 에러가 발생했습니다.");
