b38faeb548a15dcb0022b5172f8bacdc02e4c884
jjong <jj_jjong@naver.com>
2022-10-27 04:30:03 +0900

    feat: MenuController - 식단 등록 메소드 추가

--- backend/src/main/java/com/inssa/backend/menu/controller/MenuController.java
+++ backend/src/main/java/com/inssa/backend/menu/controller/MenuController.java
@@ -1,11 +1,21 @@
 package com.inssa.backend.menu.controller;
 
+import com.inssa.backend.menu.controller.dto.MenuRequest;
+import com.inssa.backend.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/menus")
 @RequiredArgsConstructor
 public class MenuController {
+
+    private final MenuService menuService;
+
+    @PostMapping
+    public ResponseEntity<Void> createMenu(@RequestHeader Long userId, @RequestBody MenuRequest menuRequest) {
+        menuService.createMenu(userId, menuRequest);
+        return ResponseEntity.ok().build();
+    }
 }
