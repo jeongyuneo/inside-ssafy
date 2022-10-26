ae125781a7e9a8e99dd89e8757a2ced1c84e0492
soobin <asdfls@naver.com>
2022-10-27 02:23:10 +0900

    feat: MemberController 회원가입 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/inssa/backend/member/controller/MemberController.java
@@ -1,9 +1,24 @@
 package com.inssa.backend.member.controller;
 
+import com.inssa.backend.member.controller.dto.MemberRequest;
+import com.inssa.backend.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<Void> join(@RequestBody MemberRequest memberRequest) {
+        memberService.join(memberRequest);
+        return ResponseEntity.ok().build();
+    }
 }
