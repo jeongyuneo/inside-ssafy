385031c67e80f486812a3bc7f7e7d448045768aa
jjong <jj_jjong@naver.com>
2022-10-27 04:46:03 +0900

    feat: MenuControllerTest - 식단 등록 성공 테스트 추가

--- backend/src/test/java/com/inssa/backend/menu/controller/MenuControllerTest.java
+++ backend/src/test/java/com/inssa/backend/menu/controller/MenuControllerTest.java
@@ -1,8 +1,68 @@
 package com.inssa.backend.menu.controller;
 
 import com.inssa.backend.ApiDocument;
+import com.inssa.backend.menu.controller.dto.MenuRequest;
+import com.inssa.backend.menu.service.MenuService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MenuController.class)
 public class MenuControllerTest extends ApiDocument {
+
+    private static final Long ID = 1L;
+    private static final String USER_ID = "userId";
+    public static final String ITEM = "코다리조림[명태:러시아산], 혼합잡곡밥, 비지찌개, 만두탕수, 상추겉절이, 포기김치";
+    public static final String DATE = "2022-10-27";
+    public static final String DAY_OF_THE_WEEK = "목";
+
+    @MockBean
+    private MenuService menuService;
+
+    private MenuRequest menuRequest;
+
+    @BeforeEach
+    void setUp() {
+        menuRequest = MenuRequest.builder()
+                .item(ITEM)
+                .date(DATE)
+                .dayOfTheWeek(DAY_OF_THE_WEEK)
+                .build();
+    }
+
+    @DisplayName("식단 등록 성공")
+    @Test
+    void create_menu_success() throws Exception {
+        // given
+        willDoNothing().given(menuService).createMenu(anyLong(), any(MenuRequest.class));
+        // when
+        ResultActions resultActions = 식단_등록_요청(menuRequest);
+        // then
+        식단_등록_성공(resultActions);
+    }
+
+    private ResultActions 식단_등록_요청(MenuRequest menuRequest) throws Exception {
+        return mockMvc.perform(post("/api/v1/menus")
+                .header(USER_ID, ID)
+                .contextPath("/api/v1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(menuRequest)));
+    }
+
+    private void 식단_등록_성공(ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(toDocument("create-menu-success"));
+    }
 }
