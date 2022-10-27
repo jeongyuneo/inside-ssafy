e7bd456bb40d8f38cccc6c1b94d4a767e5fb021b
soobin <asdfls@naver.com>
2022-10-27 22:34:28 +0900

    feat: PasswordUpdateRequest 클래스 이름 변경

--- backend/src/main/java/com/inssa/backend/member/controller/dto/PasswordUpdateRequest.java
+++ backend/src/main/java/com/inssa/backend/member/controller/dto/PasswordUpdateRequest.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberUpdateRequest {
+public class PasswordUpdateRequest {
 
     private String password;
     private String newPassword;
