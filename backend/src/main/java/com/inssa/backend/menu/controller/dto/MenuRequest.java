9f12178460e3b7b937045377fb1bc5238be2c969
jj-jjong <jj_jjong@naver.com>
2022-10-27 13:47:01 +0900

    refactor: MenuRequest - 'item' 변수 타입 수정

--- backend/src/main/java/com/inssa/backend/menu/controller/dto/MenuRequest.java
+++ backend/src/main/java/com/inssa/backend/menu/controller/dto/MenuRequest.java
@@ -2,13 +2,15 @@ package com.inssa.backend.menu.controller.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MenuRequest {
 
-    private String item;
+    private List<String> item;
     private String date;
     private String dayOfTheWeek;
 }
