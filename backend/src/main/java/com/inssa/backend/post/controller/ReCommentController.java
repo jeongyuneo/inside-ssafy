aa2951ebb6fd69a94fe6f84fb766bae898ef246c
jeonyuneo <piq2255@gmail.com>
2022-10-28 00:25:16 +0900

    feat: ReCommentController - 익명 게시판 대댓글 등록 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/controller/ReCommentController.java
+++ backend/src/main/java/com/inssa/backend/post/controller/ReCommentController.java
@@ -1,9 +1,21 @@
 package com.inssa.backend.post.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.inssa.backend.post.controller.dto.CommentRequest;
+import com.inssa.backend.post.service.ReCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/recomments")
 public class ReCommentController {
+
+    private final ReCommentService reCommentService;
+
+    @PostMapping("/{reCommentId}")
+    public ResponseEntity<Void> createReComment(@PathVariable Long reCommentId, @RequestBody CommentRequest commentRequest) {
+        reCommentService.createReComment(reCommentId, commentRequest);
+        return ResponseEntity.ok().build();
+    }
 }
