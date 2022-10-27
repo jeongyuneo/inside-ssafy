dbc16620c3f2648f41d88e62cad5505c92f9d40d
soobin <asdfls@naver.com>
2022-10-27 14:34:15 +0900

    feat: MemberResponses 추가

--- backend/src/main/java/com/inssa/backend/member/controller/dto/MemberResponses.java
+++ backend/src/main/java/com/inssa/backend/member/controller/dto/MemberResponses.java
@@ -1,4 +1,17 @@
 package com.inssa.backend.member.controller.dto;
 
+import com.inssa.backend.post.controller.dto.PostsResponse;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberResponses {
+
+    private String name;
+    private String studentNumber;
+    private List<PostsResponse> postsResponses;
 }
