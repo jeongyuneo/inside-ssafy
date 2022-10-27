1be915ece5d8026b4371bf34fbcc58ffc41d276d
jeonyuneo <piq2255@gmail.com>
2022-10-28 00:25:07 +0900

    feat: ReCommentService - 익명 게시판 대댓글 등록 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/service/ReCommentService.java
+++ backend/src/main/java/com/inssa/backend/post/service/ReCommentService.java
@@ -1,7 +1,11 @@
 package com.inssa.backend.post.service;
 
+import com.inssa.backend.post.controller.dto.CommentRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReCommentService {
+
+    public void createReComment(Long reCommentId, CommentRequest commentRequest) {
+    }
 }
