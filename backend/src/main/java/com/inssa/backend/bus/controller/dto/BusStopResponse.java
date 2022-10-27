bc086d7fcd884eb5a5aca6ef53e96f39b9ceccef
jjong <jj_jjong@naver.com>
2022-10-28 06:28:29 +0900

    feat: BusStopResponse 생성

---
+++ backend/src/main/java/com/inssa/backend/bus/controller/dto/BusStopResponse.java
@@ -0,0 +1,14 @@
+package com.inssa.backend.bus.controller.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BusStopResponse {
+
+    private String name;
+    private double latitude;
+    private double longitude;
+}
