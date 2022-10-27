464cfe2d5a584e75fc0636b255db17ddbc228299
jjong <jj_jjong@naver.com>
2022-10-28 06:29:16 +0900

    feat: BusService 버스 조회 매소드 추가

--- backend/src/main/java/com/inssa/backend/bus/service/BusService.java
+++ backend/src/main/java/com/inssa/backend/bus/service/BusService.java
@@ -1,7 +1,12 @@
 package com.inssa.backend.bus.service;
 
+import com.inssa.backend.bus.controller.dto.BusResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BusService {
+
+    public BusResponse getBus(int number) {
+        return null;
+    }
 }
