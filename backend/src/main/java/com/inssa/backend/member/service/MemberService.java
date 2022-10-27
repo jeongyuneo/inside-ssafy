620489def7b0d23925dd9f212c3ab5b0be1cc052
soobin <asdfls@naver.com>
2022-10-27 14:33:44 +0900

    feat: MemberService 회원조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/service/MemberService.java
+++ backend/src/main/java/com/inssa/backend/member/service/MemberService.java
@@ -1,11 +1,18 @@
 package com.inssa.backend.member.service;
 
 import com.inssa.backend.member.controller.dto.MemberRequest;
+import com.inssa.backend.member.controller.dto.MemberResponses;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
 
     public void join(MemberRequest memberRequest) {
+    }
+
+    public MemberResponses getMember(Long memberId) {
+        return null;
     }
 }
