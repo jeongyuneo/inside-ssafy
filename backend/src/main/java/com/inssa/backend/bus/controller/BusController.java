9dd4e0ad1eb03f21323d670f84663323b62bcd74
jjong <jj_jjong@naver.com>
2022-10-28 06:29:57 +0900

    feat: BusController 버스 조회 매소드 추가

--- backend/src/main/java/com/inssa/backend/bus/controller/BusController.java
+++ backend/src/main/java/com/inssa/backend/bus/controller/BusController.java
@@ -1,11 +1,23 @@
 package com.inssa.backend.bus.controller;
 
+import com.inssa.backend.bus.controller.dto.BusResponse;
+import com.inssa.backend.bus.service.BusService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/buses")
 public class BusController {
+
+    private final BusService busService;
+
+    @GetMapping
+    public ResponseEntity<BusResponse> getBus(@RequestParam int number) {
+        return ResponseEntity.ok().body(busService.getBus(number));
+    }
 }
