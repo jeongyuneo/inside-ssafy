6f37364082555a6f341822cb504897bb1abab4f4
jeonyuneo <piq2255@gmail.com>
2022-10-28 00:06:54 +0900

    test: ReCommentControllerTest 추가

---
+++ backend/src/test/java/com/inssa/backend/post/controller/ReCommentControllerTest.java
@@ -0,0 +1,8 @@
+package com.inssa.backend.post.controller;
+
+import com.inssa.backend.ApiDocument;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ReCommentController.class)
+public class ReCommentControllerTest extends ApiDocument {
+}
