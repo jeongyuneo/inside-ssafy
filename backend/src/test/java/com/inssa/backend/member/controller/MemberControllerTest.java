cccf9c543ba451257d98a91a581edaee5674835a
soobin <asdfls@naver.com>
2022-10-27 14:35:09 +0900

    feat: 회원조회 성공 추가

--- backend/src/test/java/com/inssa/backend/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/inssa/backend/member/controller/MemberControllerTest.java
@@ -4,7 +4,9 @@ import com.inssa.backend.ApiDocument;
 import com.inssa.backend.common.domain.ErrorMessage;
 import com.inssa.backend.common.domain.Message;
 import com.inssa.backend.member.controller.dto.MemberRequest;
+import com.inssa.backend.member.controller.dto.MemberResponses;
 import com.inssa.backend.member.service.MemberService;
+import com.inssa.backend.post.controller.dto.PostsResponse;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,9 +16,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,19 +34,39 @@ public class MemberControllerTest extends ApiDocument {
     private static final String PASSWORD = "ssafy";
     private static final String NAME = "김싸피";
     private static final String STUDENT_NUMBER = "000000";
+    private static final Long ID = 1L;
+    private static final String TITLE = "제목";
+    private static final int LIKE_COUNT = 5;
+    private static final int COMMENT_COUNT = 3;
+    private static final String CREATED_DATE = "10/25 10:19";
 
     @MockBean
     private MemberService memberService;
 
     private MemberRequest memberRequest;
 
+    private MemberResponses memberResponses;
+
     @BeforeEach
     void setUp() {
+        PostsResponse postsResponse = PostsResponse.builder()
+                .title(TITLE)
+                .likeCount(LIKE_COUNT)
+                .commentCount(COMMENT_COUNT)
+                .createdDate(CREATED_DATE)
+                .build();
         memberRequest = MemberRequest.builder()
                 .email(EMAIL)
                 .password(PASSWORD)
                 .name(NAME)
                 .studentNumber(STUDENT_NUMBER)
+                .build();
+        memberResponses = MemberResponses.builder()
+                .name(NAME)
+                .studentNumber(STUDENT_NUMBER)
+                .postsResponses(IntStream.range(0, 2)
+                        .mapToObj(n -> postsResponse)
+                        .collect(Collectors.toList()))
                 .build();
     }
 
@@ -67,6 +92,17 @@ public class MemberControllerTest extends ApiDocument {
         회원가입_실패(resultActions, new Message(ErrorMessage.FAIL_TO_JOIN));
     }
 
+    @DisplayName("회원조회 성공")
+    @Test
+    void get_member_success() throws Exception {
+        // given
+        willReturn(memberResponses).given(memberService).getMember(anyLong());
+        // when
+        ResultActions resultActions = 회원조회_요청(ID);
+        // then
+        회원조회_성공(resultActions);
+    }
+
     private ResultActions 회원가입_요청(MemberRequest memberRequest) throws Exception {
         return mockMvc.perform(post("/api/v1/members")
                 .contextPath("/api/v1")
@@ -85,5 +121,17 @@ public class MemberControllerTest extends ApiDocument {
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("join-fail"));
+    }
+
+    private ResultActions 회원조회_요청(Long memberId) throws Exception {
+        return mockMvc.perform(get("/api/v1/members/" + memberId)
+                .contextPath("/api/v1"));
+    }
+
+    private void 회원조회_성공(ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().json(toJson(memberResponses)))
+                .andDo(print())
+                .andDo(toDocument("get-member-success"));
     }
 }
