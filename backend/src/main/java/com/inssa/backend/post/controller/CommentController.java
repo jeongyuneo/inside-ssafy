a4d22eabe45e61c183ed2b11863b40d15d996e40
어정윤 <piq2255@naver.com>
2022-10-27 17:29:44 +0900

    feat: CommentController - 익명 게시판 댓글 등록 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/controller/CommentController.java
+++ backend/src/main/java/com/inssa/backend/post/controller/CommentController.java
@@ -1,9 +1,21 @@
 package com.inssa.backend.post.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.inssa.backend.post.controller.dto.CommentRequest;
+import com.inssa.backend.post.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/posts/{postId}")
+    public ResponseEntity<Void> createComment(@PathVariable Long postId, @RequestBody CommentRequest commentRequest) {
+        commentService.createComment(postId, commentRequest);
+        return ResponseEntity.ok().build();
+    }
 }
