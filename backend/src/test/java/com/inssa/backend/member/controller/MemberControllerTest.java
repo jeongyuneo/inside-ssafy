8884e9bd308f7566f3e42962d0c0c77b86087de6
soobin <asdfls@naver.com>
2022-10-28 00:16:27 +0900

    feat: 회원삭제성공 추가

--- backend/src/test/java/com/inssa/backend/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/inssa/backend/member/controller/MemberControllerTest.java
@@ -142,6 +142,17 @@ public class MemberControllerTest extends ApiDocument {
         비밀번호_수정_실패(resultActions, new Message(ErrorMessage.NOT_FOUND_MEMBER));
     }
 
+    @DisplayName("회원탈퇴 성공")
+    @Test
+    void delete_member() throws Exception {
+        // given
+        willDoNothing().given(memberService).deleteMember(anyLong());
+        // when
+        ResultActions resultActions = 회원탈퇴_요청(ID);
+        // then
+        회원탈퇴_성공(resultActions);
+    }
+
     private ResultActions 회원가입_요청(MemberRequest memberRequest) throws Exception {
         return mockMvc.perform(post("/api/v1/members")
                 .contextPath("/api/v1")
@@ -199,5 +210,16 @@ public class MemberControllerTest extends ApiDocument {
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("update-password-fail"));
+    }
+
+    private ResultActions 회원탈퇴_요청(Long memberId) throws Exception {
+        return mockMvc.perform(delete("/api/v1/members/" + memberId)
+                .contextPath("/api/v1"));
+    }
+
+    private void 회원탈퇴_성공(ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(toDocument("delete-member-success"));
     }
 }
