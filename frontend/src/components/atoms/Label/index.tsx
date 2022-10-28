6676ae0a1a301bb7385f36204b7a1d1fb27f344e
kyojun <kyojun2002@naver.com>
2022-10-28 09:21:25 +0900

    feat: 주석 변경

--- frontend/src/components/atoms/Label/index.tsx
+++ frontend/src/components/atoms/Label/index.tsx
@@ -3,7 +3,7 @@ import { StyledLabel } from './styles';
 import { PropTypes } from './types';
 
 /**
- * children은 string 또는 Icon을 받을 수 있다.
+ * children은 string을 받을 수 있다.
  * 가로, 세로, 폰트 사이즈, 글자색, 배경색을 받아 css에 적용한다.
  * label의 type을 받아 설정할 수 있다.
  * 연결할 input의 id를 받아 해당 id를 가진 input과 연결할 수 있다.
