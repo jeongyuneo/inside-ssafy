daf7072aea2d691377ae09c761a8838028c7620f
soobin <asdfls@naver.com>
2022-10-28 16:47:09 +0900

    feat: 메인페이지 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/common/service/MainService.java
+++ backend/src/main/java/com/inssa/backend/common/service/MainService.java
@@ -1,6 +1,7 @@
 package com.inssa.backend.common.service;
 
 import com.inssa.backend.common.controller.dto.FavoritesBusResponse;
+import com.inssa.backend.common.controller.dto.MainResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +9,10 @@ import java.util.List;
 @Service
 public class MainService {
     public List<FavoritesBusResponse> getFavoritesBus(Long memberId) {
+        return null;
+    }
+
+    public MainResponse getMain() {
         return null;
     }
 }
