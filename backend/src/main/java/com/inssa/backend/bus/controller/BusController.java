21624c82c183abd8ce8c88397ac8e1ccd7e724f3
soobin <asdfls@naver.com>
2022-10-28 17:57:23 +0900

    Refactor: BusService 생성

---
+++ backend/src/main/java/com/inssa/backend/bus/controller/BusController.java
@@ -0,0 +1,25 @@
+package com.inssa.backend.bus.controller;
+
+import com.inssa.backend.bus.service.BusService;
+import com.inssa.backend.bus.controller.dto.FavoritesBusResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/buses")
+public class BusController {
+
+    private final BusService busService;
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<List<FavoritesBusResponse>> getFavoritesBus(@PathVariable Long memberId) {
+        return ResponseEntity.ok().body(busService.getFavoritesBus(memberId));
+    }
+}
