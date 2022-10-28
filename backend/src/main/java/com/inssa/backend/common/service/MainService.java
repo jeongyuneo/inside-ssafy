f2bb6871bacbf0138afa9944566641a63cb4e291
soobin <asdfls@naver.com>
2022-10-28 16:11:08 +0900

    feat: 즐겨찾는 버스 메소드 추가

---
+++ backend/src/main/java/com/inssa/backend/common/service/MainService.java
@@ -0,0 +1,13 @@
+package com.inssa.backend.common.service;
+
+import com.inssa.backend.common.controller.dto.FavoritesBusResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MainService {
+    public List<FavoritesBusResponse> getFavoritesBus(Long memberId) {
+        return null;
+    }
+}
