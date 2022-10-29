268a6597212a19c6833217fd36a4c7b5cfd76218
soobin <asdfls@naver.com>
2022-10-29 12:04:21 +0900

    feat: 어노테이션 삭제

--- backend/src/main/java/com/inssa/backend/common/controller/MainController.java
+++ backend/src/main/java/com/inssa/backend/common/controller/MainController.java
@@ -5,12 +5,10 @@ import com.inssa.backend.common.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping
 public class MainController {
 
     private final MainService mainService;
