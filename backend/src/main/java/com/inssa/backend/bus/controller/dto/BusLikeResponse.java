d3dd82dff990c0d785d7e2daa7373567fc895777
soobin <asdfls@naver.com>
2022-10-29 14:31:51 +0900

    feat: 메소드명 변경

--- backend/src/main/java/com/inssa/backend/bus/controller/dto/BusLikeResponse.java
+++ backend/src/main/java/com/inssa/backend/bus/controller/dto/BusLikeResponse.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FavoriteBusesResponse {
+public class BusLikeResponse {
 
     private int number;
     private String previousBusStop;
