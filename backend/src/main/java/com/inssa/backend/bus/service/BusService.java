6faef5930e43ce8b61d1973b8051a851954212cc
jjong <jj_jjong@naver.com>
2022-10-29 13:18:27 +0900

    feat: BusService - 버스 노선 이미지 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/bus/service/BusService.java
+++ backend/src/main/java/com/inssa/backend/bus/service/BusService.java
@@ -1,6 +1,7 @@
 package com.inssa.backend.bus.service;
 
 import com.inssa.backend.bus.controller.dto.BusResponse;
+import com.inssa.backend.bus.controller.dto.RouteImageResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +12,9 @@ public class BusService {
     }
 
     public void createBusLike(Long memberId, int number) {
+    }
+
+    public RouteImageResponse getRouteImage(int number) {
+        return null;
     }
 }
