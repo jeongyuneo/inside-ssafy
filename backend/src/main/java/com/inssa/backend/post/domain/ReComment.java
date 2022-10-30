f087b21a4de87bca90426bdfff127dc27bb8baf9
jeonyuneo <piq2255@gmail.com>
2022-10-30 17:10:19 +0900

    feat: ReComment 추가

---
+++ backend/src/main/java/com/inssa/backend/post/domain/ReComment.java
@@ -0,0 +1,30 @@
+package com.inssa.backend.post.domain;
+
+import com.inssa.backend.common.domain.BaseEntity;
+import com.inssa.backend.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "recomment_id"))
+@Entity
+public class ReComment extends BaseEntity {
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+}
