748715b57000816a398c99ca94fc6d839c29c02a
jeonyuneo <piq2255@gmail.com>
2022-10-30 17:10:56 +0900

    feat: Bus 추가

---
+++ backend/src/main/java/com/inssa/backend/bus/domain/Bus.java
@@ -0,0 +1,26 @@
+package com.inssa.backend.bus.domain;
+
+import com.inssa.backend.common.domain.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "bus_id"))
+@Entity
+public class Bus extends BaseEntity {
+
+    @NotNull
+    private int number;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "bus", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Route> routes = new ArrayList<>();
+}
