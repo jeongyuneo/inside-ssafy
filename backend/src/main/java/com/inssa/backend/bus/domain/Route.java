2bf831c1de9ec46f244fcd18f1839296c0de5ae0
jeonyuneo <piq2255@gmail.com>
2022-10-30 17:09:49 +0900

    feat: Member 추가

---
+++ backend/src/main/java/com/inssa/backend/bus/domain/Route.java
@@ -0,0 +1,27 @@
+package com.inssa.backend.bus.domain;
+
+import com.inssa.backend.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "route_id"))
+@Entity
+public class Route extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bus_id")
+    private Bus bus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bus_stop_id")
+    private BusStop busStop;
+}
