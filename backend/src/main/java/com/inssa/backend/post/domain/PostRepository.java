0c2e6d68fc0504ad219ce639b5f22c0caf206ee6
soobin <asdfls@naver.com>
2022-10-31 21:52:51 +0900

    feat: 쿼리문 수정

--- backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
+++ backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
@@ -10,6 +10,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByIsActiveTrue();
 
-    @Query(value = "SELECT * FROM post WHERE title LIKE %:keyword% OR content LIKE %:keyword% ", nativeQuery = true)
+    @Query(value = "SELECT * FROM post WHERE is_active = true AND ( title LIKE %:keyword% OR content LIKE %:keyword% )", nativeQuery = true)
     List<Post> findSearchAndIsActiveTrue(@Param("keyword") String keyword);
 }
