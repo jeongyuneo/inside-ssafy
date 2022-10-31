eaafccc4bb8d8d9d75b1dee8a8164bf7341b6572
soobin <asdfls@naver.com>
2022-10-31 20:03:18 +0900

    feat: 쿼리문 추가

--- backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
+++ backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
@@ -1,6 +1,8 @@
 package com.inssa.backend.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -8,5 +10,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByIsActiveTrue();
 
-    List<Post> findByTitleContainingOrContentContaining(String title, String content);
+    @Query(value = "SELECT * FROM post WHERE title LIKE %:keyword% OR content LIKE %:keyword% ", nativeQuery = true)
+    List<Post> findSearchAndIsActiveTrue(@Param("keyword") String keyword);
 }
