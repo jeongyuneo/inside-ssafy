766a45d828157b567d15012d804b67eae541df91
jj-jjong <jj_jjong@naver.com>
2022-11-01 08:51:27 +0900

    feat: CommentRepository 삭제

--- backend/src/main/java/com/inssa/backend/post/domain/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.inssa.backend.post.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
