4ee607f0463e33154c8a72640748af842dfe38dd
soobin <asdfls@naver.com>
2022-10-31 17:06:43 +0900

    feat: 검색메소드 추가

--- backend/src/main/java/com/inssa/backend/post/service/PostService.java
+++ backend/src/main/java/com/inssa/backend/post/service/PostService.java
@@ -3,6 +3,7 @@ package com.inssa.backend.post.service;
 import com.inssa.backend.post.controller.dto.PostRequest;
 import com.inssa.backend.post.controller.dto.PostResponse;
 import com.inssa.backend.post.controller.dto.PostsResponse;
+import com.inssa.backend.post.domain.Post;
 import com.inssa.backend.post.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -31,7 +32,18 @@ public class PostService {
     }
 
     public List<PostsResponse> searchPost(String keyword) {
-        return null;
+        List<Post> posts = postRepository.findByTitleContainingOrContentContaining(keyword, keyword);
+        return posts
+                .stream()
+                .filter(Post::isActive)
+                .map(post -> PostsResponse.builder()
+                        .postId(post.getId())
+                        .title(post.getTitle())
+                        .likeCount(post.getLikeCount())
+                        .commentCount(post.getCommentCount())
+                        .createdDate(post.getCreatedDate())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     public PostResponse getPost(Long postId) {
