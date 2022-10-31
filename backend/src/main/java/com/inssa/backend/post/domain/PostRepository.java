dd3813002db8f68f7e363b0b50086e1500261fe6
soobin <asdfls@naver.com>
2022-10-31 17:06:32 +0900

    feat: 검색메소드 추가

--- backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
+++ backend/src/main/java/com/inssa/backend/post/domain/PostRepository.java
@@ -1,11 +1,12 @@
 package com.inssa.backend.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByIsActiveTrue();
+
+    List<Post> findByTitleContainingOrContentContaining(String title, String content);
 }
