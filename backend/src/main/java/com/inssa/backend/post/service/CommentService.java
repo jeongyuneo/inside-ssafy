0ae987bc6231f67c9e6c97fa060b4621c8feab7e
jj-jjong <jj_jjong@naver.com>
2022-10-31 14:59:21 +0900

    feat: CommentService - 댓글 등록 메소드 내용 작성

--- backend/src/main/java/com/inssa/backend/post/service/CommentService.java
+++ backend/src/main/java/com/inssa/backend/post/service/CommentService.java
@@ -1,17 +1,52 @@
 package com.inssa.backend.post.service;
 
+import com.inssa.backend.common.domain.ErrorMessage;
+import com.inssa.backend.common.exception.NotFoundException;
+import com.inssa.backend.member.domain.Member;
+import com.inssa.backend.member.domain.MemberRepository;
 import com.inssa.backend.post.controller.dto.CommentRequest;
+import com.inssa.backend.post.domain.Comment;
+import com.inssa.backend.post.domain.CommentRepository;
+import com.inssa.backend.post.domain.Post;
+import com.inssa.backend.post.domain.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CommentService {
 
-    public void createComment(Long postId, CommentRequest commentRequest) {
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
+    public void createComment(Long memberId, Long postId, CommentRequest commentRequest) {
+        Member member = findMember(memberId);
+        Post post = findPost(postId);
+        commentRepository.save(
+                Comment.builder()
+                        .content(commentRequest.getContent())
+                        .member(member)
+                        .post(post)
+                        .build()
+        );
+        post.addComment();
+        postRepository.save(post);
     }
 
     public void updateComment(Long commentId, CommentRequest commentRequest) {
     }
 
     public void deleteComment(Long commentId) {
+    }
+
+    private Member findMember(Long memberId) {
+        return memberRepository.findByIdAndIsActiveTrue(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_MEMBER));
+    }
+
+    private Post findPost(Long postId) {
+        return postRepository.findByIdAndIsActiveTrue(postId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_POST));
     }
 }
