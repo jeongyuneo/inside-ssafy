67b69745c6a001944297a57f55dbdf0c840612ec
kyojun <kyojun2002@naver.com>
2022-10-31 15:50:33 +0900

    feat : App.tsx 초기값으로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import InputLabel from './components/molecules/InputLabel';
 
 function App() {
-  return (
-    <div className="App">
-      <InputLabel
-        id="name"
-        width={20}
-        height={2}
-        borderRadius={0.3}
-        labelFontSize={0.5}
-        inputFontSize={0.8}
-        paddingLeft={0.5}
-      >
-        테스트라벨
-      </InputLabel>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
