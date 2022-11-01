a5133f367a9bd15f2360b915a9de44cdedcacaa5
jj-jjong <jj_jjong@naver.com>
2022-11-01 15:07:04 +0900

    feat: CommentService - 댓글 삭제 메소드 구현

--- backend/src/main/java/com/inssa/backend/post/service/CommentService.java
+++ backend/src/main/java/com/inssa/backend/post/service/CommentService.java
@@ -2,10 +2,12 @@ package com.inssa.backend.post.service;
 
 import com.inssa.backend.common.domain.ErrorMessage;
 import com.inssa.backend.common.exception.NotFoundException;
+import com.inssa.backend.common.exception.UnAuthorizedException;
 import com.inssa.backend.member.domain.Member;
 import com.inssa.backend.member.domain.MemberRepository;
 import com.inssa.backend.post.controller.dto.CommentRequest;
 import com.inssa.backend.post.domain.Comment;
+import com.inssa.backend.post.domain.CommentRepository;
 import com.inssa.backend.post.domain.Post;
 import com.inssa.backend.post.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
+    private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
 
@@ -33,7 +36,16 @@ public class CommentService {
     public void updateComment(Long commentId, CommentRequest commentRequest) {
     }
 
-    public void deleteComment(Long commentId) {
+    public void deleteComment(Long memberId, Long commentId) {
+        Comment comment = findComment(commentId);
+        checkEditable(findMember(memberId), comment);
+        comment.delete();
+        commentRepository.save(comment);
+    }
+
+    private Comment findComment(Long commentId) {
+        return commentRepository.findByIdAndIsActiveTrue(commentId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_COMMENT));
     }
 
     private Member findMember(Long memberId) {
@@ -44,5 +56,11 @@ public class CommentService {
     private Post findPost(Long postId) {
         return postRepository.findByIdAndIsActiveTrue(postId)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_POST));
+    }
+
+    private void checkEditable(Member member, Comment comment) {
+        if (!member.isEditable(comment.getMember().getId())) {
+            throw new UnAuthorizedException(ErrorMessage.NOT_ALLOWED_MEMBER);
+        }
     }
 }
