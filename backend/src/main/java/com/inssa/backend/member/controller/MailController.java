9aa8637155b62dc3b6617b9b53dcac70c1cbfeb9
jjong <jj_jjong@naver.com>
2022-11-02 06:44:50 +0900

    feat: MailController - 인증 메일 전송 & 검증 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/controller/MailController.java
+++ backend/src/main/java/com/inssa/backend/member/controller/MailController.java
@@ -1,11 +1,27 @@
 package com.inssa.backend.member.controller;
 
+import com.inssa.backend.member.controller.dto.EmailAuthRequest;
+import com.inssa.backend.member.service.MailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/mails")
 public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendEmailValidationToken(@RequestParam String email) {
+        mailService.sendEmailValidationToken(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<Void> validateSignUpEmail(@RequestBody EmailAuthRequest emailAuthRequest) {
+        mailService.validateSignUpEmail(emailAuthRequest);
+        return ResponseEntity.ok().build();
+    }
 }
