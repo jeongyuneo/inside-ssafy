ba5b515cdd95ce143348e520996de2c198ddb87b
soobin <asdfls@naver.com>
2022-11-01 14:46:27 +0900

    feat: 식단 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/menu/service/MenuService.java
+++ backend/src/main/java/com/inssa/backend/menu/service/MenuService.java
@@ -1,16 +1,41 @@
 package com.inssa.backend.menu.service;
 
+import com.inssa.backend.menu.controller.dto.ItemsResponse;
 import com.inssa.backend.menu.controller.dto.MenuRequest;
 import com.inssa.backend.menu.controller.dto.MenuResponse;
+import com.inssa.backend.menu.domain.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class MenuService {
+
+    private static final int MONDAY = 0;
+    private static final int FRIDAY = 4;
+
+    private final MenuRepository menuRepository;
 
     public void createMenu(Long userId, MenuRequest menuRequest) {
     }
 
-    public MenuResponse getMenu(String date) {
-        return null;
+    public MenuResponse getMenu() {
+        List<ItemsResponse> itemsResponses = menuRepository.findByIsActiveTrue()
+                .stream()
+                .map(menu -> ItemsResponse.builder()
+                        .date(menu.getDate())
+                        .dayOfTheWeek(menu.getDayOfTheWeek())
+                        .items(Arrays.stream(menu.getItem().split(", ")).collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+        return MenuResponse.builder()
+                .startDate(itemsResponses.get(MONDAY).getDate())
+                .endDate(itemsResponses.get(FRIDAY).getDate())
+                .menus(itemsResponses)
+                .build();
     }
 }
