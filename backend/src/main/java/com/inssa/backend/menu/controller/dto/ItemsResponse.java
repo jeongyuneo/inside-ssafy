d11e137f4e912fa8f0dc5b64a93219a000dbcf57
soobin <asdfls@naver.com>
2022-11-01 14:44:02 +0900

    feat: ItemResponse 클래스 추가

---
+++ backend/src/main/java/com/inssa/backend/menu/controller/dto/ItemsResponse.java
@@ -0,0 +1,16 @@
+package com.inssa.backend.menu.controller.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemsResponse {
+
+    private String date;
+    private String dayOfTheWeek;
+    private List<String> items;
+}
