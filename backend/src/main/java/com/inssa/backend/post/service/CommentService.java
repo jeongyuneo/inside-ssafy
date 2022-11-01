8614a8d66339d907a61104aeef0d8a99e30543f0
jj-jjong <jj_jjong@naver.com>
2022-11-01 11:29:40 +0900

    feat: CommentService - 댓글 수정 메소드 내용 작성

--- backend/src/main/java/com/inssa/backend/post/service/CommentService.java
+++ backend/src/main/java/com/inssa/backend/post/service/CommentService.java
@@ -1,17 +1,49 @@
 package com.inssa.backend.post.service;
 
+import com.inssa.backend.common.domain.ErrorMessage;
+import com.inssa.backend.common.exception.NotFoundException;
+import com.inssa.backend.common.exception.UnAuthorizedException;
+import com.inssa.backend.member.domain.Member;
+import com.inssa.backend.member.domain.MemberRepository;
 import com.inssa.backend.post.controller.dto.CommentRequest;
+import com.inssa.backend.post.domain.Comment;
+import com.inssa.backend.post.domain.CommentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
 
     public void createComment(Long postId, CommentRequest commentRequest) {
     }
 
-    public void updateComment(Long commentId, CommentRequest commentRequest) {
+    public void updateComment(Long memberId, Long commentId, CommentRequest commentRequest) {
+        Comment comment = findComment(commentId);
+        checkEditable(findMember(memberId), comment);
+        comment.update(commentRequest.getContent());
+        commentRepository.save(comment);
     }
 
     public void deleteComment(Long commentId) {
+    }
+
+    private Comment findComment(Long commentId) {
+        return commentRepository.findByIdAndIsActiveTrue(commentId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_COMMENT));
+    }
+
+    private Member findMember(Long memberId) {
+        return memberRepository.findByIdAndIsActiveTrue(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_MEMBER));
+    }
+
+    private void checkEditable(Member member, Comment comment) {
+        if (!member.isEditable(comment.getMember().getId())) {
+            throw new UnAuthorizedException(ErrorMessage.NOT_ALLOWED_MEMBER);
+        }
     }
 }
