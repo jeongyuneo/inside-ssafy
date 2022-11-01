6330f44875e5274a4fdd38ac67a2a04ef512d30f
soobin <asdfls@naver.com>
2022-11-01 14:43:36 +0900

    feat: 데이터 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,20 @@
-INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count) VALUES (now(), now(), 1, '제목', '본문', 1, 2);
-INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count) VALUES (now(), now(), 1, '문제23', '본문265456', 2, 3);
-INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count) VALUES (now(), now(), 0, '아리랑', '본문1111', 2, 3);
-INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count) VALUES (now(), now(), 0, '오늘 점심 존맛', '본문0986', 2, 3);
-INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count) VALUES (now(), now(), 0, '제목은 왜 제목', '본문0986', 2, 3);
+INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count)
+VALUES (now(), now(), 1, '제목', '본문', 1, 2);
+INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count)
+VALUES (now(), now(), 1, '문제23', '본문265456', 2, 3);
+INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count)
+VALUES (now(), now(), 0, '아리랑', '본문1111', 2, 3);
+INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count)
+VALUES (now(), now(), 0, '오늘 점심 존맛', '본문0986', 2, 3);
+INSERT INTO post(created_date, last_modified_date, is_active, title, content, comment_count, like_count)
+VALUES (now(), now(), 0, '제목은 왜 제목', '본문0986', 2, 3);
+INSERT INTO menu(created_date, last_modified_date, is_active, item, menu_date, day_of_the_week)
+VALUES (now(), now(), 1, '치즈함박스테이크 , 쌀밥/크림스프, 푸실리샐러드, 오이피클&할라피뇨 , 그린샐러드, 포기김치, 숭늉', '2022-10-31', '월요일');
+INSERT INTO menu(created_date, last_modified_date, is_active, item, menu_date, day_of_the_week)
+VALUES (now(), now(), 1, '유산슬덮밥 , 계란팟국, 깐풍기, 쑥갓사과무침, 포기김치 , 샐러드바/숭늉', '2022-11-01', '화요일');
+INSERT INTO menu(created_date, last_modified_date, is_active, item, menu_date, day_of_the_week)
+VALUES (now(), now(), 1, '잔치국수, 추가밥, 너비아니조림, 진미채채소무침, 물방개묵무침, 포기김치', '2022-11-02', '수요일');
+INSERT INTO menu(created_date, last_modified_date, is_active, item, menu_date, day_of_the_week)
+VALUES (now(), now(), 1, '돼지불고기, 혼합잡곡밥, 시금치된장국, 미역줄기팽이볶음 ,무짠지무침, 포기김치', '2022-11-03', '목요일');
+INSERT INTO menu(created_date, last_modified_date, is_active, item, menu_date, day_of_the_week)
+VALUES (now(), now(), 1, '사골떡국, 추가밥, 생선가스, 타르타르소스, 두부조림, 상추겉절이, 깍두기', '2022-11-04', '금요일');
