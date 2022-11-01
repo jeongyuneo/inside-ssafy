84b889a270ed8652e530da5d87aa6e4acf2d70da
jjong <jj_jjong@naver.com>
2022-11-02 06:43:27 +0900

    feat: MailController 생성

---
+++ backend/src/main/java/com/inssa/backend/member/controller/MailController.java
@@ -0,0 +1,11 @@
+package com.inssa.backend.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/mails")
+public class MailController {
+}
