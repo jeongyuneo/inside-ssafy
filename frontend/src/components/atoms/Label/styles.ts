2d09513faeec83eb2615c0afb93a4f4976bdc418
kyojun <kyojun2002@naver.com>
2022-11-01 16:58:16 +0900

    style : label textcolor 디폴트색상 변경

--- frontend/src/components/atoms/Label/styles.ts
+++ frontend/src/components/atoms/Label/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import { PropTypes } from './types';
 
 const DEFAULT_FONTSIZE = 1;
-const DEFAULT_TEXTCOLOR = 'red';
+const DEFAULT_TEXTCOLOR = 'black';
 export const StyledLabel = styled.label<PropTypes>`
   ${({ fontSize }) => css`
     font-size: ${fontSize || DEFAULT_FONTSIZE}rem;
