9a992977611b0dad4c3ba8813bda0f593163fc37
jeonyuneo <piq2255@gmail.com>
2022-11-02 11:34:45 +0900

    feat: JwtUtil 추가

---
+++ backend/src/main/java/com/inssa/backend/util/JwtUtil.java
@@ -0,0 +1,76 @@
+package com.inssa.backend.util;
+
+import com.inssa.backend.common.domain.ErrorMessage;
+import com.inssa.backend.common.exception.UnAuthorizedException;
+import com.inssa.backend.member.domain.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JwtUtil {
+
+    private static final String JWT_HEADER = "Authorization";
+    private static final String SECRET = "ssafy second semester third project - inssa";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 60 * 1000L;
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 14 * 1000L;
+    private static final String AUTHORIZATION_TYPE = "Bearer";
+    private static final String DELIMITER = " ";
+    private static final int TOKEN = 1;
+    private static final String ID = "id";
+
+    public static String generateToken(Long memberId, Role role) {
+        Claims claims = Jwts.claims();
+        claims.put("id", memberId);
+        claims.put("role", role);
+        return createToken(claims, ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    public static String generateToken(String email) {
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+        return createToken(claims, REFRESH_TOKEN_EXPIRE_TIME);
+    }
+
+    public static int getMemberId(String token) {
+        return (int) getAllClaims(getActualToken(token)).get(ID);
+    }
+
+    private static String createToken(Claims claims, long expireTime) {
+        return Jwts.builder()
+                .setSubject(JWT_HEADER)
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expireTime))
+                .signWith(SignatureAlgorithm.HS256, SECRET.getBytes(StandardCharsets.UTF_8))
+                .compact();
+    }
+
+    private static Claims getAllClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(SECRET.getBytes(StandardCharsets.UTF_8))
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new UnAuthorizedException(ErrorMessage.EXPIRED_TOKEN);
+        }
+    }
+
+    private static String getActualToken(String token) {
+        validateAuthorization(token);
+        return token.split(DELIMITER)[TOKEN];
+    }
+
+    private static void validateAuthorization(String token) {
+        if (!token.startsWith(AUTHORIZATION_TYPE + DELIMITER)) {
+            throw new UnAuthorizedException(ErrorMessage.WRONG_TOKEN);
+        }
+    }
+}
