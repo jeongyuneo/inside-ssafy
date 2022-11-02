525036c1c84b7004eedbe62f70d1b2d2ab4116db
jj-jjong <jj_jjong@naver.com>
2022-11-02 15:37:10 +0900

    feat: ErrorMessage - 인증코드 전송 실패 & 만료 메시지 추가

--- backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
@@ -16,18 +16,20 @@ public enum ErrorMessage {
     NOT_FOUND_ROUTE_IMAGE("해당 노선 이미지를 찾을 수 없습니다."),
     NOT_FOUND_ROUTE("해당 경로를 찾을 수 없습니다."),
 
+    NOT_EQUAL_VALIDATION_TOKEN("인증코드가 일치하지 않습니다."),
     NOT_EQUAL_PASSWORD("비밀번호가 일치하지 않습니다."),
-    NOT_EQUAL_VALIDATION_TOKEN("인증 코드가 일치하지 않습니다."),
-
 
     NOT_EDITABLE_MEMBER("편집 권한이 없는 회원입니다."),
 
+    FAIL_TO_SEND_VALIDATION_TOKEN("인증코드 전송에 실패했습니다."),
     FAIL_TO_JOIN("회원 가입에 실패했습니다."),
     FAIL_TO_GET_POSTS("익명 게시판 목록 조회에 실패했습니다."),
     FAIL_TO_SEARCH_POST("익명 게시판 검색에 실패했습니다."),
     FAIL_TO_CREATE_POST("익명 게시판 등록에 실패했습니다."),
     FAIL_TO_CREATE_COMMENT("익명 게시판 댓글 등록에 실패했습니다."),
     FAIL_TO_CREATE_MENU("식단 등록에 실패했습니다."),
+
+    EXPIRED_VALIDATION_TOKEN("인증코드가 만료되었습니다."),
 
     INTERNAL_SERVER_ERROR("서버 내부 에러가 발생했습니다.");
 
