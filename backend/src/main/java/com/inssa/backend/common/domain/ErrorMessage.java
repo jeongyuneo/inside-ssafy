c522a8e5338361f901bbf4ef1278f5e95af07cf7
jeonyuneo <piq2255@gmail.com>
2022-11-02 11:35:10 +0900

    feat: ErrorMessage - 토큰 관련 에러 메시지 추가

--- backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/inssa/backend/common/domain/ErrorMessage.java
@@ -20,6 +20,11 @@ public enum ErrorMessage {
 
     NOT_EDITABLE_MEMBER("편집 권한이 없는 회원입니다."),
 
+    WRONG_TOKEN("잘못된 토큰입니다."),
+    WRONG_TOKEN_TYPE("잘못된 토큰 타입입니다."),
+
+    EXPIRED_TOKEN("만료된 토큰입니다."),
+
     FAIL_TO_JOIN("회원 가입에 실패했습니다."),
     FAIL_TO_GET_POSTS("익명 게시판 목록 조회에 실패했습니다."),
     FAIL_TO_SEARCH_POST("익명 게시판 검색에 실패했습니다."),
