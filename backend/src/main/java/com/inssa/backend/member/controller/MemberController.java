bf0c62eb14b5f77212e6fd84d0b296029951058e
jeonyuneo <piq2255@gmail.com>
2022-11-02 11:33:22 +0900

    feat: MemberController - 로그인 jwt 토큰 적용

--- backend/src/main/java/com/inssa/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/inssa/backend/member/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.inssa.backend.member.controller;
 
 import com.inssa.backend.member.controller.dto.*;
 import com.inssa.backend.member.service.MemberService;
+import com.inssa.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,6 +41,14 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.ok().body(memberService.login(loginRequest));
+        ResponseCookie responseCookie = ResponseCookie.from("refreshToken", JwtUtil.generateToken(loginRequest.getEmail()))
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(60 * 60 * 24 * 14)
+                .domain("inside-ssafy.com")
+                .build();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, responseCookie.toString()).body(memberService.login(loginRequest));
     }
 }
