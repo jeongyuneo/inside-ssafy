d3babfa50a491b08e7e3ad46c982bb43bc20eb36
jjong <jj_jjong@naver.com>
2022-11-02 21:25:47 +0900

    feat: ReCommentRepository 삭제

--- backend/src/main/java/com/inssa/backend/post/domain/ReCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.inssa.backend.post.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReCommentRepository extends JpaRepository<ReComment, Long> {
-}
