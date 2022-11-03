7d9f5d78348b925971255a68db264bcb961382ee
jj-jjong <jj_jjong@naver.com>
2022-11-03 10:30:27 +0900

    feat: ReCommentRepository 생성

---
+++ backend/src/main/java/com/inssa/backend/post/domain/ReCommentRepository.java
@@ -0,0 +1,6 @@
+package com.inssa.backend.post.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReCommentRepository extends JpaRepository<ReComment, Long> {
+}
