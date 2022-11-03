a6dae1bd2ddadb27b41ee20111ace25b540f84e6
jeonyuneo <piq2255@gmail.com>
2022-11-03 14:26:21 +0900

    feat: WebConfig - 인터셉터 추가

--- backend/src/main/java/com/inssa/backend/config/WebConfig.java
+++ backend/src/main/java/com/inssa/backend/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.inssa.backend.config;
 
+import com.inssa.backend.interceptor.JwtAuthFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .maxAge(6000);
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new JwtAuthFilter())
+                .excludePathPatterns(EXCLUDE_PATH_PATTERNS);
     }
 }
