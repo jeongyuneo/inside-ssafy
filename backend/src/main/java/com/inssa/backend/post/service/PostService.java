490f76c5c2c2a5cf636f701a80625a715fe8d0b0
soobin <asdfls@naver.com>
2022-11-03 10:16:31 +0900

    test: 편집 권한 예외 타입 수정

--- backend/src/main/java/com/inssa/backend/post/service/PostService.java
+++ backend/src/main/java/com/inssa/backend/post/service/PostService.java
@@ -1,8 +1,8 @@
 package com.inssa.backend.post.service;
 
 import com.inssa.backend.common.domain.ErrorMessage;
+import com.inssa.backend.common.exception.ForbiddenException;
 import com.inssa.backend.common.exception.NotFoundException;
-import com.inssa.backend.common.exception.UnAuthorizedException;
 import com.inssa.backend.member.domain.Member;
 import com.inssa.backend.member.domain.MemberRepository;
 import com.inssa.backend.post.controller.dto.PostRequest;
@@ -96,7 +96,7 @@ public class PostService {
 
     private void checkEditable(Member member, Post post) {
         if (!member.isEditable(post.getMember().getId())) {
-            throw new UnAuthorizedException(ErrorMessage.NOT_EDITABLE_MEMBER);
+            throw new ForbiddenException(ErrorMessage.NOT_EDITABLE_MEMBER);
         }
     }
 }
