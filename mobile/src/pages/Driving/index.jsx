4c32795d6aa30eb1dadbfebfb22bb1954852a8d9
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-11-03 16:51:20 +0900

    feat: navigation prop 적용

--- mobile/src/pages/Driving/index.jsx
+++ mobile/src/pages/Driving/index.jsx
@@ -1,13 +1,19 @@
-import { StyledDriving, MainText, StyledButton, TitleText } from "./styles";
+import { useEffect, useState } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { StyledDriving, MainText, StyledButton, TitleText } from "./styles";
 
 /**
  * navigation을 받아서 페이지 이동에 사용한다.
  * 운행종료 버튼을 클릭시 메인페이지로 이동한다.
+ * route로 Main 페이지에서 props를 받는다.
  *
  * author jini
  */
-const Driving = ({ navigation }) => {
+const Driving = ({ navigation, route }) => {
+  useEffect(() => {
+    console.log(route.params.busInfo);
+  }, []);
+
   const movePage = () => {
     navigation.navigate("Main");
   };
