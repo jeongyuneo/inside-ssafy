cc0607b9b0191e303dcb974f84b33705917603c8
jj-jjong <jj_jjong@naver.com>
2022-11-03 13:30:34 +0900

    feat: ReComment - 편집 권한 확인 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/domain/ReComment.java
+++ backend/src/main/java/com/inssa/backend/post/domain/ReComment.java
@@ -27,4 +27,8 @@ public class ReComment extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id")
     private Comment comment;
+
+    public boolean isEditable(Long memberId) {
+        return this.member.is(memberId);
+    }
 }
