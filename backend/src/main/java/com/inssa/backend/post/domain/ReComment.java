e13cc5944475cfc5779e3ca8f76f49cfb23063be
jj-jjong <jj_jjong@naver.com>
2022-11-03 10:02:21 +0900

    feat: ReComment - 대댓글 수정 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/domain/ReComment.java
+++ backend/src/main/java/com/inssa/backend/post/domain/ReComment.java
@@ -27,4 +27,8 @@ public class ReComment extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id")
     private Comment comment;
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
