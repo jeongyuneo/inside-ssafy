019b1e15828209f35adf147a9d6551a4b27c65eb
jj-jjong <jj_jjong@naver.com>
2022-11-03 10:01:35 +0900

    feat: ReCommentRepository - reCommentId로 대댓글 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/post/domain/ReCommentRepository.java
+++ backend/src/main/java/com/inssa/backend/post/domain/ReCommentRepository.java
@@ -2,5 +2,8 @@ package com.inssa.backend.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReCommentRepository extends JpaRepository<ReComment, Long> {
+    Optional<ReComment> findByIdAndIsActiveTrue(Long reCommentId);
 }
