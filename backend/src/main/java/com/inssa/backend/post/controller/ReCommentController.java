d937c86c455a452da8b9c7af03de414998a306fd
jeonyuneo <piq2255@gmail.com>
2022-11-03 14:54:44 +0900

    feat: ReCommentController - jwt 로직 적용

--- backend/src/main/java/com/inssa/backend/post/controller/ReCommentController.java
+++ backend/src/main/java/com/inssa/backend/post/controller/ReCommentController.java
@@ -2,6 +2,7 @@ package com.inssa.backend.post.controller;
 
 import com.inssa.backend.post.controller.dto.CommentRequest;
 import com.inssa.backend.post.service.ReCommentService;
+import com.inssa.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +16,13 @@ public class ReCommentController {
 
     @PostMapping("/{reCommentId}")
     public ResponseEntity<Void> createReComment(@RequestHeader("Authorization") String token, @PathVariable Long reCommentId, @RequestBody CommentRequest commentRequest) {
-        reCommentService.createReComment(1L, reCommentId, commentRequest);
+        reCommentService.createReComment(JwtUtil.getMemberId(token), reCommentId, commentRequest);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{reCommentId}")
     public ResponseEntity<Void> updateReComment(@RequestHeader("Authorization") String token, @PathVariable Long reCommentId, @RequestBody CommentRequest commentRequest) {
-        reCommentService.updateReComment(1L, reCommentId, commentRequest);
+        reCommentService.updateReComment(JwtUtil.getMemberId(token), reCommentId, commentRequest);
         return ResponseEntity.ok().build();
     }
 
