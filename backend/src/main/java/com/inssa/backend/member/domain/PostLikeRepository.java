43fcf6c9fed72244015bc6fc9600177302b7b416
jjong <jj_jjong@naver.com>
2022-11-04 00:15:53 +0900

    feat: PostLikeRepository - MemberAndPost로 게시글 좋아요 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/domain/PostLikeRepository.java
+++ backend/src/main/java/com/inssa/backend/member/domain/PostLikeRepository.java
@@ -1,6 +1,11 @@
 package com.inssa.backend.member.domain;
 
+import com.inssa.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByMemberAndPostAndIsActiveTrue(Member member, Post post);
 }
