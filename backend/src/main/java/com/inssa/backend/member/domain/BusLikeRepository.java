af47bddfbd2114be7db64c7714c173ee9fc6f718
jeonyuneo <piq2255@gmail.com>
2022-11-04 21:34:57 +0900

    feat: BusLikeRepository 추가

---
+++ backend/src/main/java/com/inssa/backend/member/domain/BusLikeRepository.java
@@ -0,0 +1,6 @@
+package com.inssa.backend.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusLikeRepository extends JpaRepository<BusLike, Long> {
+}
