f56a5f45c57a035c8a137a88a0cba20ded0d551d
어정윤 <piq2255@naver.com>
2022-11-04 17:04:32 +0900

    feat: PostLikeRepository - existsByMemberAndPostAndIsActiveTrue 메소드 파라미터 순서 수정

--- backend/src/main/java/com/inssa/backend/member/domain/PostLikeRepository.java
+++ backend/src/main/java/com/inssa/backend/member/domain/PostLikeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
-    boolean existsByPostAndMemberAndIsActiveTrue(Post post, Member member);
+    boolean existsByMemberAndPostAndIsActiveTrue(Member member, Post port);
 }
