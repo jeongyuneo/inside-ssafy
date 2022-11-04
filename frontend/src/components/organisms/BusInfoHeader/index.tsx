1dd1b3797ee07d9ea09eb27d3257464948cbc69c
seonghun <pch468@naver.com>
2022-11-04 17:37:42 +0900

    feat: BusInfoHeader organism 컴포넌트 추가

---
+++ frontend/src/components/organisms/BusInfoHeader/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import BusInfoNavbar from '../../molecules/BusInfoNavbar';
+import BusInfoNavigator from '../../molecules/BusInfoNavigator';
+import Navbar from '../../molecules/Navbar';
+import { StyledBusInfoHeader } from './styles';
+import { PropTypes } from './types';
+
+/**
+ * BusLine과 새로고침 버튼을 렌더링
+ *
+ * @author jojo
+ */
+const BusInfoHeader = ({
+  busNum,
+  liked,
+  clickLogoHandler,
+  clickBusNumHandler,
+  toggleLikeHandler,
+  toggleBusInfoModalHandler,
+}: PropTypes) => {
+  return (
+    <StyledBusInfoHeader>
+      <Navbar clickLogoHandler={clickLogoHandler} />
+      <BusInfoNavigator
+        busNum={busNum}
+        clickBusNumHandler={clickBusNumHandler}
+      />
+      <BusInfoNavbar
+        liked={liked}
+        toggleLikeHandler={toggleLikeHandler}
+        toggleBusInfoModalHandler={toggleBusInfoModalHandler}
+      />
+    </StyledBusInfoHeader>
+  );
+};
+
+export default BusInfoHeader;
---
+++ frontend/src/components/organisms/BusInfoHeader/types.ts
@@ -0,0 +1,8 @@
+import { PropTypes as NavbarPropTypes } from '../../molecules/Navbar/types';
+import { PropTypes as BusInfoNavigatorPropTypes } from '../../molecules/BusInfoNavigator/types';
+import { PropTypes as BusInfoNavbarPropTypes } from '../../molecules/BusInfoNavbar/types';
+
+export interface PropTypes
+  extends NavbarPropTypes,
+    BusInfoNavigatorPropTypes,
+    BusInfoNavbarPropTypes {}
