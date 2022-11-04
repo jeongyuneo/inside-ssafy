149d43b5c65669edefece2c23a07daa41e3e0e5b
어정윤 <piq2255@naver.com>
2022-11-04 16:57:18 +0900

    feat: RouteRepository - findByAndIsActiveTrue 추가

--- backend/src/main/java/com/inssa/backend/bus/domain/RouteRepository.java
+++ backend/src/main/java/com/inssa/backend/bus/domain/RouteRepository.java
@@ -2,5 +2,9 @@ package com.inssa.backend.bus.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    Optional<Route> findByIdAndIsActiveTrue(Long routeId);
 }
