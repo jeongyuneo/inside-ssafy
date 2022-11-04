f281ec2791f7bc7a6c75c4bdcd545a02ad99c6c9
jeonyuneo <piq2255@gmail.com>
2022-11-04 21:36:34 +0900

    feat: BusLikeRepository - findByMemberAndBusAndIsActiveFalse 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/domain/BusLikeRepository.java
+++ backend/src/main/java/com/inssa/backend/member/domain/BusLikeRepository.java
@@ -3,9 +3,13 @@ package com.inssa.backend.member.domain;
 import com.inssa.backend.bus.domain.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BusLikeRepository extends JpaRepository<BusLike, Long> {
 
     boolean existsByMemberAndBusAndIsActiveTrue(Member member, Bus bus);
 
     boolean existsByMemberAndBusAndIsActiveFalse(Member member, Bus bus);
+
+    Optional<BusLike> findByMemberAndBusAndIsActiveFalse(Member member, Bus bus);
 }
