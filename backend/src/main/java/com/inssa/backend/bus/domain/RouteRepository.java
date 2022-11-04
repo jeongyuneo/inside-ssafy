727dbaaf1676e3ed277ad8ed00a76f867ea1b97b
jj-jjong <jj_jjong@naver.com>
2022-11-04 15:16:37 +0900

    feat: RouteRepository - Bus로 노선 조회 메소드 추가

--- backend/src/main/java/com/inssa/backend/bus/domain/RouteRepository.java
+++ backend/src/main/java/com/inssa/backend/bus/domain/RouteRepository.java
@@ -2,5 +2,9 @@ package com.inssa.backend.bus.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    List<Route> findByBusOrderByOrderAsc(Bus bus);
 }
