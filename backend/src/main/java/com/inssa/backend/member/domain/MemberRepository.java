8b5d552b7e7c17d94410ac30e5b917e38dd5e550
jj-jjong <jj_jjong@naver.com>
2022-11-06 21:29:13 +0900

    feat: MemberRepository - 이메일 중복체크 메소드 추가

--- backend/src/main/java/com/inssa/backend/member/domain/MemberRepository.java
+++ backend/src/main/java/com/inssa/backend/member/domain/MemberRepository.java
@@ -9,4 +9,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByIdAndIsActiveTrue(Long memberId);
 
     Optional<Member> findByEmailAndIsActiveTrue(String email);
+
+    boolean existsByEmail(String email);
 }
