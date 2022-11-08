35bc90b3d5ca72d4d045333a4b26f378b4116035
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-11-08 17:47:36 +0900

    feat: MyPage UI 구현

--- frontend/src/components/pages/MyPage/styles.ts
+++ frontend/src/components/pages/MyPage/styles.ts
@@ -4,13 +4,5 @@ export const StyledMyPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 3rem;
-`;
-
-export const PostSummaryWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
   gap: 1rem;
 `;
