db72b0831a7f1382f1c234eabc8d8eb538c8c619
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-11-08 13:48:58 +0900

    feat: MyPage에 PostSummary 적용

--- frontend/src/components/pages/MyPage/index.tsx
+++ frontend/src/components/pages/MyPage/index.tsx
@@ -1,20 +1,60 @@
 import React from 'react';
 import MyInfo from '../../organisms/MyInfo';
+import PostSummary from '../../molecules/PostSummary';
+import { StyledMyPage, PostSummaryWrapper } from './styles';
 
 const MyPage = () => {
   const name = '홍길동';
   const studentNumber = '0732206';
+  const postInfos = [
+    {
+      postId: 1,
+      title: '게시글 제목1',
+      likeCount: 3,
+      commentCount: 5,
+      createdDate: '2022-11-08 09:12',
+    },
+    {
+      postId: 2,
+      title: '게시글 제목2',
+      likeCount: 200,
+      commentCount: 70,
+      createdDate: '2022-11-08 01:35',
+    },
+    {
+      postId: 3,
+      title: '게시글 제목3',
+      likeCount: 5,
+      commentCount: 19,
+      createdDate: '2022-11-08 11:18',
+    },
+  ];
 
   const clickEditBtnHandler = () => {
     console.log('button clicked');
   };
 
   return (
-    <MyInfo
-      name={name}
-      studentNumber={studentNumber}
-      clickEditBtnHandler={clickEditBtnHandler}
-    />
+    <StyledMyPage>
+      <MyInfo
+        name={name}
+        studentNumber={studentNumber}
+        clickEditBtnHandler={clickEditBtnHandler}
+      />
+      <PostSummaryWrapper>
+        {postInfos.map(
+          ({ postId, title, likeCount, commentCount, createdDate }) => (
+            <PostSummary
+              key={postId}
+              title={title}
+              date={createdDate}
+              likeCount={likeCount.toString()}
+              commentCount={commentCount.toString()}
+            />
+          ),
+        )}
+      </PostSummaryWrapper>
+    </StyledMyPage>
   );
 };
 
