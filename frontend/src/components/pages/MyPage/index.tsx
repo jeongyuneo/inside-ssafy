6202a43d028ad360fe2be4a15e48ea14f0a17aae
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-11-08 14:02:23 +0900

    feat: MyPage에서 PostSummary 제거

--- frontend/src/components/pages/MyPage/index.tsx
+++ frontend/src/components/pages/MyPage/index.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import MyInfo from '../../organisms/MyInfo';
-import PostSummary from '../../molecules/PostSummary';
-import { StyledMyPage, PostSummaryWrapper } from './styles';
+import { StyledMyPage } from './styles';
 
 const MyPage = () => {
   const name = '홍길동';
   const studentNumber = '0732206';
-  const postInfos = [
-    {
-      postId: 1,
-      title: '게시글 제목1',
-      likeCount: 3,
-      commentCount: 5,
-      createdDate: '2022-11-08 09:12',
-    },
-    {
-      postId: 2,
-      title: '게시글 제목2',
-      likeCount: 200,
-      commentCount: 70,
-      createdDate: '2022-11-08 01:35',
-    },
-    {
-      postId: 3,
-      title: '게시글 제목3',
-      likeCount: 5,
-      commentCount: 19,
-      createdDate: '2022-11-08 11:18',
-    },
-  ];
 
   const clickEditBtnHandler = () => {
     console.log('button clicked');
@@ -41,19 +17,6 @@ const MyPage = () => {
         studentNumber={studentNumber}
         clickEditBtnHandler={clickEditBtnHandler}
       />
-      <PostSummaryWrapper>
-        {postInfos.map(
-          ({ postId, title, likeCount, commentCount, createdDate }) => (
-            <PostSummary
-              key={postId}
-              title={title}
-              date={createdDate}
-              likeCount={likeCount.toString()}
-              commentCount={commentCount.toString()}
-            />
-          ),
-        )}
-      </PostSummaryWrapper>
     </StyledMyPage>
   );
 };
