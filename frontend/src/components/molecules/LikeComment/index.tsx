844282cbb7bab73335061b28670645ce0ff9a29b
허혜진 <hyejin@heohyejin-ui-MacBookPro.local>
2022-11-08 10:10:48 +0900

    design: 아이콘 색상 변경

--- frontend/src/components/molecules/LikeComment/index.tsx
+++ frontend/src/components/molecules/LikeComment/index.tsx
@@ -14,14 +14,14 @@ const LikeComment = ({ likeCount, commentCount }: PropTypes) => {
   return (
     <StyledLikeComment>
       <TextWrapper>
-        <FaRegThumbsUp color="red" size={15} />
-        <Text color={'red'} size={0.9}>
+        <FaRegThumbsUp color="#D55555" size={15} />
+        <Text color="#D55555" size={0.9}>
           {likeCount}
         </Text>
       </TextWrapper>
       <TextWrapper>
-        <FaRegCommentDots color="#1E90FF" size={15} />
-        <Text color={'#1E90FF'} size={0.9}>
+        <FaRegCommentDots color="#5B9CCE" size={15} />
+        <Text color="#5B9CCE" size={0.9}>
           {commentCount}
         </Text>
       </TextWrapper>
