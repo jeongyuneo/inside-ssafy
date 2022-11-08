77d5ce295c6c69110f461970fa213bb9c65a7d14
kyojun <kyojun2002@naver.com>
2022-11-08 14:40:57 +0900

    feat : 메뉴페이지 추가중

---
+++ frontend/src/components/pages/Menu/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MenuWeekWrapper = styled.div``;
+
+export const MenuCardsWrapper = styled.div``;
