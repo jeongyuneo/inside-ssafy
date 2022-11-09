b25c54fa82c784f3c6ea1499f337ff25c09a379c
Hyejin <heo015414@gmail.com>
2022-11-09 16:59:46 +0900

    refactor: imageTextInfos 분리

---
+++ frontend/src/components/pages/Main/getImageInfos.ts
@@ -0,0 +1,40 @@
+import { NavigateFunction } from 'react-router-dom';
+
+export const getImageInfos = (navigate: NavigateFunction) => {
+  const movePage = (address: string) => {
+    navigate(`/${address}`);
+  };
+
+  const moveNewLocation = () => {
+    window.open('https://edu.ssafy.com');
+  };
+
+  const imageTextInfos = [
+    {
+      src: './images/eduSSAFY.png',
+      alt: 'edussafy',
+      clickImageHandler: () => moveNewLocation(),
+      children: '에듀싸피',
+    },
+    {
+      src: './images/tayo_bus.png',
+      alt: 'bus',
+      clickImageHandler: () => movePage('businfo'),
+      children: '셔틀 정보',
+    },
+    {
+      src: './images/speaker.png',
+      alt: 'speaker',
+      clickImageHandler: () => movePage('myPage'),
+      children: '익명게시판',
+    },
+    {
+      src: './images/plate.png',
+      alt: 'plate',
+      clickImageHandler: () => movePage('myPage'),
+      children: '식단표',
+    },
+  ];
+
+  return imageTextInfos;
+};
--- frontend/src/components/pages/Main/index.tsx
+++ frontend/src/components/pages/Main/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import clickBusNumHandler from '../../../utils/clickBusNumHandler';
 import FavoriteBusCarosel from '../../organisms/FavoriteBusCarosel';
 import ImageTextButtonGroup from '../../organisms/ImageTextButtonGroup';
+import { getImageInfos } from './getImageInfos';
 
 const Main = () => {
   const [busNum, setBusNum] = useState(1);
@@ -20,44 +21,9 @@ const Main = () => {
     clickBusNumHandler({ direction, busNum, setBusNum });
   };
 
-  const movePage = (address: string) => {
-    navigate(`/${address}`);
-  };
-
-  const moveNewLocation = () => {
-    window.open('https://edu.ssafy.com');
-  };
-
-  const imageTextInfos = [
-    {
-      src: './images/eduSSAFY.png',
-      alt: 'edussafy',
-      clickImageHandler: () => moveNewLocation(),
-      children: '에듀싸피',
-    },
-    {
-      src: './images/tayo_bus.png',
-      alt: 'bus',
-      clickImageHandler: () => movePage('businfo'),
-      children: '셔틀 정보',
-    },
-    {
-      src: './images/speaker.png',
-      alt: 'speaker',
-      clickImageHandler: () => movePage('myPage'),
-      children: '익명게시판',
-    },
-    {
-      src: './images/plate.png',
-      alt: 'plate',
-      clickImageHandler: () => movePage('myPage'),
-      children: '식단표',
-    },
-  ];
-
   return (
     <>
-      <ImageTextButtonGroup imageTextInfos={imageTextInfos} />
+      <ImageTextButtonGroup imageTextInfos={getImageInfos(navigate)} />
       <FavoriteBusCarosel
         busNum={busNum}
         previousBusStop="충대 정문"
