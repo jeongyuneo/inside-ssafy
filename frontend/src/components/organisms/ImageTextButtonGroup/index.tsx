ef9155371d3814a14853acd724e53c5deba3b017
Hyejin <heo015414@gmail.com>
2022-11-09 15:10:52 +0900

    refactor: ImageTextButtonGroup 에서 상태 제거

--- frontend/src/components/organisms/ImageTextButtonGroup/index.tsx
+++ frontend/src/components/organisms/ImageTextButtonGroup/index.tsx
@@ -1,61 +1,27 @@
 import React from 'react';
 import ImageTextButton from '../../molecules/ImageTextButton';
 import { StyledImageTextButtonGroup } from './styles';
-import { useNavigate } from 'react-router-dom';
+import { ImageTextButtonGroupTypes } from './types';
 
 /**
  * imageInfos 정보에 따라 화면을 렌더링합니다.
  *
  * @author jini
  */
-const ImageTextButtonGroup = () => {
-  const navigate = useNavigate();
-  const movePage = (address: string) => {
-    navigate(`/${address}`);
-  };
-
-  const moveNewLocation = () => {
-    window.open('https://edu.ssafy.com');
-  };
-
-  const imageInfos = [
-    {
-      src: './images/eduSSAFY.png',
-      alt: 'edussafy',
-      clickHandler: () => moveNewLocation(),
-      text: '에듀싸피',
-    },
-    {
-      src: './images/tayo_bus.png',
-      alt: 'bus',
-      clickHandler: () => movePage('businfo'),
-      text: '셔틀 정보',
-    },
-    {
-      src: './images/speaker.png',
-      alt: 'speaker',
-      clickHandler: () => movePage('myPage'),
-      text: '익명게시판',
-    },
-    {
-      src: './images/plate.png',
-      alt: 'plate',
-      clickHandler: () => movePage('myPage'),
-      text: '식단표',
-    },
-  ];
-
+const ImageTextButtonGroup = ({
+  imageTextInfos,
+}: ImageTextButtonGroupTypes) => {
   return (
     <StyledImageTextButtonGroup>
-      {imageInfos.map(({ src, alt, clickHandler, text }) => {
+      {imageTextInfos.map(({ src, alt, clickImageHandler, children }) => {
         return (
           <ImageTextButton
             key={src}
             src={src}
             alt={alt}
-            clickImageHandler={clickHandler}
+            clickImageHandler={clickImageHandler}
           >
-            {text}
+            {children}
           </ImageTextButton>
         );
       })}
--- frontend/src/components/pages/Main/index.tsx
+++ frontend/src/components/pages/Main/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import clickBusNumHandler from '../../../utils/clickBusNumHandler';
 import FavoriteBusCarosel from '../../organisms/FavoriteBusCarosel';
 import ImageTextButtonGroup from '../../organisms/ImageTextButtonGroup';
 
 const Main = () => {
   const [busNum, setBusNum] = useState(1);
+  const navigate = useNavigate();
 
   const clickRefreshHandler = () => {
     console.log('refresh');
@@ -18,9 +20,44 @@ const Main = () => {
     clickBusNumHandler({ direction, busNum, setBusNum });
   };
 
+  const movePage = (address: string) => {
+    navigate(`/${address}`);
+  };
+
+  const moveNewLocation = () => {
+    window.open('https://edu.ssafy.com');
+  };
+
+  const imageTextInfos = [
+    {
+      src: './images/eduSSAFY.png',
+      alt: 'edussafy',
+      clickImageHandler: () => moveNewLocation(),
+      children: '에듀싸피',
+    },
+    {
+      src: './images/tayo_bus.png',
+      alt: 'bus',
+      clickImageHandler: () => movePage('businfo'),
+      children: '셔틀 정보',
+    },
+    {
+      src: './images/speaker.png',
+      alt: 'speaker',
+      clickImageHandler: () => movePage('myPage'),
+      children: '익명게시판',
+    },
+    {
+      src: './images/plate.png',
+      alt: 'plate',
+      clickImageHandler: () => movePage('myPage'),
+      children: '식단표',
+    },
+  ];
+
   return (
     <>
-      <ImageTextButtonGroup />
+      <ImageTextButtonGroup imageTextInfos={imageTextInfos} />
       <FavoriteBusCarosel
         busNum={busNum}
         previousBusStop="충대 정문"
