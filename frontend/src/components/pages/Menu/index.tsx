fcb8db235175b99551448a24629b45a8729bd6be
kyojun <kyojun2002@naver.com>
2022-11-09 17:17:21 +0900

    feat : react query 적용준비중

--- frontend/src/components/pages/Menu/index.tsx
+++ frontend/src/components/pages/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import MenuWeek from '../../molecules/MenuWeek';
 import MenuCards from '../../organisms/MenuCards';
@@ -12,26 +13,28 @@ const Menu = () => {
     startDate: '0000-00-00',
     endDate: '0000-00-00',
   };
-  const [menuOfTheWeek, setMenuOfTheWeek] = useState(DEFAULT_STATE);
+  const [menuOfWeek, setMenuOfWeek] = useState(DEFAULT_STATE);
   const getMenuOfTheWeek = async () => {
     const data = await requestMenu();
-    setMenuOfTheWeek(data);
+    setMenuOfWeek(data);
   };
 
   useEffect(() => {
     getMenuOfTheWeek();
   }, []);
 
+  const menuOfTheWeek = useQuery<MenuTypes>([''], () => requestMenu());
+
   return (
     <StyledMenu>
       <MenuWeekWrapper>
         <MenuWeek
-          startDate={menuOfTheWeek.startDate}
-          endDate={menuOfTheWeek.endDate}
+          startDate={menuOfWeek.startDate}
+          endDate={menuOfWeek.endDate}
         ></MenuWeek>
       </MenuWeekWrapper>
       <MenuCardsWrapper>
-        <MenuCards menus={menuOfTheWeek.menus}></MenuCards>
+        <MenuCards menus={menuOfWeek.menus}></MenuCards>
       </MenuCardsWrapper>
     </StyledMenu>
   );
