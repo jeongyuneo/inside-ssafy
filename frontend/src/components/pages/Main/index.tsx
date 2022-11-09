d06ab38274b28a685c0d4d3122bb930ee7eec612
seonghun <pch468@naver.com>
2022-11-09 11:08:51 +0900

    feat: Main 페이지 상태 추가, 캐러셀 선언

--- frontend/src/components/pages/Main/index.tsx
+++ frontend/src/components/pages/Main/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clickBusNumHandler from '../../../utils/clickBusNumHandler';
+import FavoriteBusCarosel from '../../organisms/FavoriteBusCarosel';
 import ImageTextButtonGroup from '../../organisms/ImageTextButtonGroup';
 
 const Main = () => {
-  return <ImageTextButtonGroup />;
+  const [busNum, setBusNum] = useState(1);
+
+  const clickRefreshHandler = () => {
+    console.log('refresh');
+  };
+
+  const clickPlusHandler = () => {
+    console.log('plus');
+  };
+
+  const clickBusNumHandlerWrapper = (direction: string) => {
+    clickBusNumHandler({ direction, busNum, setBusNum });
+  };
+
+  return (
+    <>
+      <ImageTextButtonGroup />
+      <FavoriteBusCarosel
+        busNum={busNum}
+        previousBusStop="충대 정문"
+        nextBusStop="유성온천역 7번 출구"
+        clickRefreshHandler={clickRefreshHandler}
+        clickPlusHandler={clickPlusHandler}
+        clickBusNumHandler={clickBusNumHandlerWrapper}
+      />
+    </>
+  );
 };
 
 export default Main;
