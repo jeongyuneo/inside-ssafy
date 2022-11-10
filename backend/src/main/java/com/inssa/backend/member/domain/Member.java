8e6b1fe7ea8cae9067e2d707cd199f17749b5f4f
jeonyuneo <piq2255@gmail.com>
2022-11-10 10:23:23 +0900

    refactor: Member - 메소드 선언 순서 수정

--- backend/src/main/java/com/inssa/backend/member/domain/Member.java
+++ backend/src/main/java/com/inssa/backend/member/domain/Member.java
@@ -57,15 +57,15 @@ public class Member extends BaseEntity {
         }
     }
 
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
     public boolean is(Long memberId) {
         return id.equals(memberId);
     }
 
     public void addBusLike(BusLike busLike) {
         busLikes.add(busLike);
-    }
-
-    public void updatePassword(String password) {
-        this.password = password;
     }
 }
