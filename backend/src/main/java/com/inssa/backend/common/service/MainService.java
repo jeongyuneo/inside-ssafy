c95747b7b9b8cc3189a856d765459967448d0cb5
jjong <jj_jjong@naver.com>
2022-11-10 18:09:58 +0900

    feat: MainService - 메인페이지 메소드 식단 조회 수정

--- backend/src/main/java/com/inssa/backend/common/service/MainService.java
+++ backend/src/main/java/com/inssa/backend/common/service/MainService.java
@@ -1,17 +1,21 @@
 package com.inssa.backend.common.service;
 
 import com.inssa.backend.common.controller.dto.MainResponse;
+import com.inssa.backend.common.controller.dto.MenuResponse;
 import com.inssa.backend.common.domain.ErrorMessage;
+import com.inssa.backend.common.exception.BadRequestException;
 import com.inssa.backend.common.exception.NotFoundException;
 import com.inssa.backend.member.domain.BusLike;
 import com.inssa.backend.member.domain.Member;
 import com.inssa.backend.member.domain.MemberRepository;
+import com.inssa.backend.menu.domain.Menu;
 import com.inssa.backend.menu.domain.MenuRepository;
 import com.inssa.backend.post.controller.dto.PostsResponse;
 import com.inssa.backend.post.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -29,6 +33,7 @@ public class MainService {
     private final PostRepository postRepository;
 
     public MainResponse getMain(Long memberId) {
+        Menu menu = findMenuOfToday();
         return MainResponse.builder()
                 .busLikes(findMember(memberId).getBusLikes()
                         .stream()
@@ -36,10 +41,10 @@ public class MainService {
                         .sorted(Comparator.comparing(current -> current.getBus().getNumber()))
                         .map(busLikes -> busLikes.getBus().getNumber())
                         .collect(Collectors.toList()))
-                .menus(Arrays.stream(menuRepository.findByDateEqualsAndIsActiveTrue(LocalDate.now())
-                                .getItem()
-                                .split(DELIMITER))
-                        .collect(Collectors.toList()))
+                .menu(MenuResponse.builder()
+                        .items(Arrays.stream(menu.getItem().split(DELIMITER)).collect(Collectors.toList()))
+                        .subItems(Arrays.stream(menu.getSubItem().split(DELIMITER)).collect(Collectors.toList()))
+                        .build())
                 .hotPosts(postRepository.findTop5ByIsActiveTrueAndLikeCountGreaterThanEqualOrderByCreatedDateDesc(HOT_POST_STANDARD)
                         .stream()
                         .map(post -> PostsResponse.builder()
@@ -56,5 +61,14 @@ public class MainService {
     private Member findMember(Long memberId) {
         return memberRepository.findByIdAndIsActiveTrue(memberId)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_MEMBER));
+    }
+
+    private Menu findMenuOfToday() {
+        LocalDate today = LocalDate.now();
+        if (today.getDayOfWeek().equals(DayOfWeek.SATURDAY) || today.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            throw new BadRequestException(ErrorMessage.NOT_WEEKDAY);
+        }
+        return menuRepository.findByDateEqualsAndIsActiveTrue(today)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_MENU));
     }
 }
