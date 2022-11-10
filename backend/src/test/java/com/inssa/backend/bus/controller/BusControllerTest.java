b3788b3a1467fc9a0f802225f6611dd440d7ad49
jeonyuneo <piq2255@gmail.com>
2022-11-10 10:52:19 +0900

    test: BusControllerTest - 버스 즐겨찾기 등록 파라미터 수정

--- backend/src/test/java/com/inssa/backend/bus/controller/BusControllerTest.java
+++ backend/src/test/java/com/inssa/backend/bus/controller/BusControllerTest.java
@@ -1,10 +1,7 @@
 package com.inssa.backend.bus.controller;
 
 import com.inssa.backend.ApiDocument;
-import com.inssa.backend.bus.controller.dto.BusLikeResponse;
-import com.inssa.backend.bus.controller.dto.BusResponse;
-import com.inssa.backend.bus.controller.dto.RouteImageResponse;
-import com.inssa.backend.bus.controller.dto.RouteResponse;
+import com.inssa.backend.bus.controller.dto.*;
 import com.inssa.backend.bus.service.BusService;
 import com.inssa.backend.common.domain.ErrorMessage;
 import com.inssa.backend.common.domain.Message;
@@ -16,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
@@ -51,6 +49,7 @@ public class BusControllerTest extends ApiDocument {
     @MockBean
     private BusService busService;
 
+    private BusRequest busRequest;
     private BusResponse busResponse;
     private List<BusLikeResponse> busLikeResponses;
     private RouteImageResponse routeImageResponse;
@@ -67,6 +66,9 @@ public class BusControllerTest extends ApiDocument {
                 .routeId(ID)
                 .name(NAME)
                 .address(ADDRESS)
+                .build();
+        busRequest = BusRequest.builder()
+                .number(NUMBER)
                 .build();
         busResponse = BusResponse.builder()
                 .isLast(IS_LAST)
@@ -112,7 +114,7 @@ public class BusControllerTest extends ApiDocument {
     @Test
     void create_bus_like_success() throws Exception {
         // given
-        willDoNothing().given(busService).createBusLike(anyLong(), anyInt());
+        willDoNothing().given(busService).createBusLike(anyLong(), any(BusRequest.class));
         // when
         ResultActions resultActions = 버스_즐겨찾기_등록_요청(NUMBER);
         // then
@@ -123,7 +125,7 @@ public class BusControllerTest extends ApiDocument {
     @Test
     void create_bus_like_fail() throws Exception {
         // given
-        willThrow(new NotFoundException(ErrorMessage.NOT_FOUND_BUS)).given(busService).createBusLike(anyLong(), anyInt());
+        willThrow(new NotFoundException(ErrorMessage.NOT_FOUND_BUS)).given(busService).createBusLike(anyLong(), any(BusRequest.class));
         // when
         ResultActions resultActions = 버스_즐겨찾기_등록_요청(NUMBER);
         // then
@@ -265,7 +267,8 @@ public class BusControllerTest extends ApiDocument {
         return mockMvc.perform(post("/api/v1/buses/like")
                 .contextPath("/api/v1")
                 .header(AUTHORIZATION, BEARER + ACCESS_TOKEN)
-                .param(NUMBER_PARAMETER_NAME, String.valueOf(number)));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(busRequest)));
     }
 
     private void 버스_즐겨찾기_등록_성공(ResultActions resultActions) throws Exception {
