f227c98d47127c38b99f3097b0ace0c9e66e29e3
jjong <jj_jjong@naver.com>
2022-11-10 15:47:16 +0900

    feat: MainController - 메인페이지 조회 메소드 수정

--- backend/src/main/java/com/inssa/backend/common/controller/MainController.java
+++ backend/src/main/java/com/inssa/backend/common/controller/MainController.java
@@ -2,9 +2,11 @@ package com.inssa.backend.common.controller;
 
 import com.inssa.backend.common.controller.dto.MainResponse;
 import com.inssa.backend.common.service.MainService;
+import com.inssa.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,7 +16,7 @@ public class MainController {
     private final MainService mainService;
 
     @GetMapping
-    public ResponseEntity<MainResponse> getMain() {
-        return ResponseEntity.ok().body(mainService.getMain());
+    public ResponseEntity<MainResponse> getMain(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok().body(mainService.getMain(JwtUtil.getMemberId(token)));
     }
 }
