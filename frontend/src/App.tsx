09328166ed76ff4ca193280749a30ad527ce15de
kyojun <kyojun2002@naver.com>
2022-11-10 11:00:30 +0900

    feat : conflict 해결 시도 , App변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,42 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BusInfo from './components/pages/BusInfo';
 import Join from './components/pages/Join';
 import Login from './components/pages/Login';
 import MyPage from './components/pages/MyPage';
 import Main from './components/pages/Main';
-import Menu from './components/pages/Menu';
+import Spinner from './utils/Spinner';
+import { ErrorBoundary } from './utils/ErrorBoundary';
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  });
+
   axios.defaults.baseURL = 'https://www.inside-ssafy.com/';
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/businfo" element={<BusInfo />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <Suspense fallback={<Spinner />}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/join" element={<Join />} />
+              <Route path="/businfo" element={<BusInfo />} />
+              <Route path="/mypage" element={<MyPage />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
