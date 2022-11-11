d947ee8d978bcb2ff4cc659daa921c8d2a872247
kyojun <kyojun2002@naver.com>
2022-11-11 09:28:48 +0900

    feat: PostHeader type 지정

--- frontend/src/components/molecules/PostHeader/styled.ts
+++ frontend/src/components/molecules/PostHeader/styled.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const StyledPostHeader = styled.div``;
+export const StyledPostHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PostHeadWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ButtonWrapper = styled.div``;
