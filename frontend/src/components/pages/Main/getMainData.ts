651634e5bdfb24cdae8b8ee12322e689ae2b2a89
seonghun <pch468@naver.com>
2022-11-14 17:08:46 +0900

    feat: 에러 처리 로직 수정

--- frontend/src/components/pages/Main/getMainData.ts
+++ frontend/src/components/pages/Main/getMainData.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { MainDataType } from './types';
 
 const getMainData = async () => {
@@ -8,16 +8,12 @@ const getMainData = async () => {
       url: '/api/v1',
     });
 
+    console.log();
+
     return data;
   } catch (e) {
-    // 주말에는 식단표를 제공하지 않아 400 에러
-    if (e instanceof AxiosError && e?.response?.status === 400) {
-      console.log(e?.response?.data);
-      return e?.response?.data.message;
-    } else {
-      console.log(e);
-      throw e;
-    }
+    console.log(e);
+    throw e;
   }
 };
 
