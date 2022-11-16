82cd111110245ee615b5a6893b329d0372095e83
kyojun <kyojun2002@naver.com>
2022-11-16 14:45:15 +0900

    feat: 코멘트 반영

--- frontend/src/components/molecules/BoardNavbar/styles.ts
+++ frontend/src/components/molecules/BoardNavbar/styles.ts
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const StyledBoardNavbar = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 100%;
-  padding: 0 1rem;
+  width: 90%;
 `;
