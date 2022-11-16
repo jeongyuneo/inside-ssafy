0cf3e50a70677a99c083d8e94b01578591b3da3c
kyojun <kyojun2002@naver.com>
2022-11-16 11:30:27 +0900

    feat : BoardNavbar 작성

---
+++ frontend/src/components/molecules/BoardNavbar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../../atoms/Button';
+import { StyledBoardNavbar } from './styles';
+import { PropTypes } from './types';
+import { BiSearchAlt } from 'react-icons/bi';
+
+const BoardNavbar = ({
+  clickBackButtonHandler,
+  clickSearchButtonHandler,
+}: PropTypes) => {
+  return (
+    <StyledBoardNavbar>
+      <Button fontSize={1} clickHandler={clickBackButtonHandler} isText>
+        뒤로가기
+      </Button>
+      <Button clickHandler={clickSearchButtonHandler} isText>
+        <BiSearchAlt size={25} />
+      </Button>
+    </StyledBoardNavbar>
+  );
+};
+
+export default BoardNavbar;
---
+++ frontend/src/components/molecules/BoardNavbar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledBoardNavbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 1rem;
+`;
