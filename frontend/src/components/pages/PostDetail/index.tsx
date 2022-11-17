1fafb65427d80837871ccc0a8d30747d60832d55
seonghun <pch468@naver.com>
2022-11-17 17:56:53 +0900

    feat: PostDetail 페이지 수정

--- frontend/src/components/pages/PostDetail/index.tsx
+++ frontend/src/components/pages/PostDetail/index.tsx
@@ -1,8 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import CommentSection from '../../organisms/CommentSection';
+import PostSection from '../../organisms/PostSection';
 import getPostDetail from './getPostDetail';
-import { StyledPostDetail } from './styles';
+import { HorizontalLineWrapper, StyledPostDetail } from './styles';
+import { PostDetailTypes } from './types';
+import navigator from '../../../utils/navigator';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../../molecules/Navbar';
+import HorizontalLine from '../../../utils/HorizontalLine';
 
 /**
  * 게시글 상세 컴포넌트
@@ -10,7 +16,19 @@ import { StyledPostDetail } from './styles';
  * @author jojo
  */
 const PostDetail = () => {
-  const { data } = useQuery(['postDetail'], () => getPostDetail(1));
+  const navigate = useNavigate();
+
+  const { data: post } = useQuery<PostDetailTypes>(['postDetail'], () =>
+    getPostDetail(1),
+  );
+  console.log(post);
+
+  const clickMenuButtonHandler = () => {
+    console.log('menu click');
+  };
+  const clickLikeButtonHandler = () => {
+    console.log('like!');
+  };
 
   const clickReCommentHandler = () => {
     console.log('recomment!');
@@ -20,11 +38,36 @@ const PostDetail = () => {
   };
   return (
     <StyledPostDetail>
-      <CommentSection
-        commentResponses={data.commentResponses}
-        clickReCommentHandler={clickReCommentHandler}
-        clickDeleteHandler={clickDeleteHandler}
-      />
+      {post && (
+        <>
+          <Navbar
+            clickLogoHandler={navigator(navigate).main}
+            clickMypageHandler={navigator(navigate).mypage}
+          />
+          <PostSection
+            content={post.content}
+            likeCount={post.likeCount}
+            commentCount={post.commentCount}
+            files={post.files}
+            postId={post.postId}
+            title={post.title}
+            createdDate={post.createdDate}
+            editable={post.editable}
+            hasPostLike={post.hasPostLike}
+            clickBackButtonHandler={navigator(navigate).back}
+            clickMenuButtonHandler={clickMenuButtonHandler}
+            clickLikeButtonHandler={clickLikeButtonHandler}
+          />
+          <HorizontalLineWrapper>
+            <HorizontalLine width="90%" />
+          </HorizontalLineWrapper>
+          <CommentSection
+            commentResponses={post.commentResponses}
+            clickReCommentHandler={clickReCommentHandler}
+            clickDeleteHandler={clickDeleteHandler}
+          />
+        </>
+      )}
     </StyledPostDetail>
   );
 };
--- frontend/src/components/pages/PostDetail/styles.ts
+++ frontend/src/components/pages/PostDetail/styles.ts
@@ -3,4 +3,11 @@ import styled from 'styled-components';
 export const StyledPostDetail = styled.div`
   display: flex;
   flex-direction: column;
+  background: linear-gradient(rgb(12, 235, 176, 0.14), white);
+`;
+
+export const HorizontalLineWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
