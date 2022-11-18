b43f59e0109b035f07e2cce2bf4e6b8e2e66c88a
seonghun <pch468@naver.com>
2022-11-18 15:12:09 +0900

    feat: Textarea 컴포넌트 width, height 문자열로 받기, disable, defaultValue 속성 추가

--- frontend/src/components/atoms/Textarea/types.ts
+++ frontend/src/components/atoms/Textarea/types.ts
@@ -3,11 +3,13 @@ import { ChangeEvent, ChangeEventHandler } from 'react';
 export interface PropTypes {
   id: string;
   name: string;
-  width?: number;
-  height?: number;
+  defaultValue?: string;
+  width?: string;
+  height?: string;
   fontSize?: number;
   backgroundColor?: string;
   placeholder?: string;
+  disabled?: boolean;
   changeHandler?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
   onChange?: ChangeEventHandler<HTMLTextAreaElement>;
 }
