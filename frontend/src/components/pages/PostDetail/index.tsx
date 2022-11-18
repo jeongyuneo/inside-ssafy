5c9340150560fc0b67aa596c89bd9622335f791c
seonghun <pch468@naver.com>
2022-11-18 13:24:42 +0900

    feat: 댓글 추가/삭제 함수 추가, 대댓글 달기 클릭 유무 구별하는 상태 추가, CommentInput 상태, 변경함수 추가

--- frontend/src/components/pages/PostDetail/index.tsx
+++ frontend/src/components/pages/PostDetail/index.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import CommentSection from '../../organisms/CommentSection';
 import PostSection from '../../organisms/PostSection';
 import getPostDetail from './getPostDetail';
 import { StyledPostDetail } from './styles';
 import { PostDetailTypes } from './types';
 import navigator from '../../../utils/navigator';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Navbar from '../../molecules/Navbar';
 import deletePostLike from './deletePostLike';
 import postPostLike from './postPostLike';
+import deleteRecomment from './deleteRecomment';
+import deleteComment from './deleteComment';
+import useFocus from '../../../hooks/useFocus';
+import postComment from './postComment';
+import postRecomment from './postRecomment';
 
 /**
  * 게시글 상세 컴포넌트
@@ -17,13 +22,22 @@ import postPostLike from './postPostLike';
  * @author jojo
  */
 const PostDetail = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const postId = 1;
+  const location = useLocation();
+  const postId: number = location.state.postId;
 
+  const [commentIdWritingRecomment, setCommentIdWritingRecomment] =
+    useState(-1);
   const [postLiked, setPostLiked] = useState(false);
+  const [inputs, setInputs] = useState({
+    comment: '',
+  });
+  const { ref, setIsFocused } = useFocus();
 
-  const { data: post } = useQuery<PostDetailTypes>(['postDetail'], () =>
-    getPostDetail(postId),
+  const { data: post } = useQuery<PostDetailTypes>(
+    ['postDetail', postLiked],
+    () => getPostDetail(postId),
   );
   console.log(post);
 
@@ -31,11 +45,41 @@ const PostDetail = () => {
     console.log('menu click');
   };
 
-  const clickReCommentHandler = () => {
-    console.log('recomment!');
+  const clickReCommentHandler = (commentId: number) => {
+    setIsFocused(prev => !prev);
+    commentIdWritingRecomment === -1
+      ? setCommentIdWritingRecomment(commentId)
+      : setCommentIdWritingRecomment(-1);
   };
-  const clickDeleteHandler = () => {
-    console.log('delete!');
+
+  const clickDeleteHandler = (commentId: number, isReComment: boolean) => {
+    if (confirm('삭제하시겠습니까?')) {
+      isReComment ? deleteRecomment(commentId) : deleteComment(commentId);
+    }
+  };
+
+  const clickSubmitHandler = async () => {
+    if (inputs.comment.trim() === '') {
+      return;
+    }
+
+    commentIdWritingRecomment === -1
+      ? await postComment(postId, inputs.comment)
+      : await postRecomment(commentIdWritingRecomment, inputs.comment);
+
+    queryClient.invalidateQueries(['postDetail', postLiked]);
+
+    setInputs(prev => ({
+      ...prev,
+      comment: '',
+    }));
+  };
+
+  const changeCommentInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs(prev => ({
+      ...prev,
+      comment: e.target.value,
+    }));
   };
 
   const togglePostLikeHandler = async () => {
@@ -74,9 +118,14 @@ const PostDetail = () => {
             togglePostLikeHandler={togglePostLikeHandler}
           />
           <CommentSection
+            ref={ref}
+            inputs={inputs}
             commentResponses={post.commentResponses}
+            commentIdWritingRecomment={commentIdWritingRecomment}
             clickReCommentHandler={clickReCommentHandler}
             clickDeleteHandler={clickDeleteHandler}
+            clickSubmitHandler={clickSubmitHandler}
+            changeCommentInputHandler={changeCommentInputHandler}
           />
         </>
       )}
