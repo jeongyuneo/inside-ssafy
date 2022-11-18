b09a5590957d54b0d303f7755348cda2e532d319
seonghun <pch468@naver.com>
2022-11-18 18:23:54 +0900

    feat: Textarea 컨텐츠에 맞게 높이 조정

--- frontend/src/components/atoms/Textarea/index.tsx
+++ frontend/src/components/atoms/Textarea/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyledTextarea } from './styles';
 import { PropTypes } from './types';
 
@@ -9,10 +9,19 @@ import { PropTypes } from './types';
  * @author jini
  */
 const Textarea = ({ id, name, changeHandler, ...rest }: PropTypes) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.style.height = ref.current.scrollHeight + 'px';
+    }
+  }, []);
+
   return (
     <StyledTextarea
       id={id}
       name={name}
+      ref={ref}
       onChange={e => changeHandler?.(e)}
       {...rest}
     />
