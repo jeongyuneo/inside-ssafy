8cb77d28fb4d7c59667510eb9552e61ecc159497
seonghun <pch468@naver.com>
2022-11-20 00:16:10 +0900

    style: LikeComment 사이즈 조정

--- frontend/src/components/molecules/LikeComment/styles.ts
+++ frontend/src/components/molecules/LikeComment/styles.ts
@@ -2,14 +2,10 @@ import styled from 'styled-components';
 
 export const StyledLikeComment = styled.div`
   display: flex;
-  width: 6rem;
-  justify-content: end;
   gap: 0.5rem;
 `;
 
 export const TextWrapper = styled.div`
   display: flex;
-  width: 40%;
-  justify-content: end;
   align-items: center;
 `;
